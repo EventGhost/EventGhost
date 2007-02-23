7f7865c287501ae926101605f645a24b1e307962
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2007-02-23 22:28:28 +0000

    Moved Joystick PluginInfo to from __info__.py to __init__.py

--- plugins/Joystick/__info__.py
+++
@@ -1,8 +0,0 @@
-name = "Joystick"
-author = "Bitmonster"
-version = "0.0.1"
-kind = "remote"
-description = """
-Makes it possible to use joysticks and gamepads as input devices 
-for EventGhost.
-"""
--- plugins/Joystick/__init__.py
+++ plugins/Joystick/__init__.py
@@ -1,4 +1,16 @@
 import eg
+
+class PluginInfo(eg.PluginInfo):
+    name = "Joystick"
+    author = "Bitmonster"
+    version = "0.0.1"
+    kind = "remote"
+    description = (
+        "Makes it possible to use joysticks and gamepads as input devices "
+        "for EventGhost."
+    )   
+
+
 import os
 
 EVT_DIRECTION       = 0
