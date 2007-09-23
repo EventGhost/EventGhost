3526a67104d753edcb2a5b0c5c339470339d6d83
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2007-09-23 12:06:46 +0000

    Combined Fhz1000Pc plugins files into one file.

--- plugins/Fhz1000Pc/__init__.py
+++ plugins/Fhz1000Pc/__init__.py
@@ -8,8 +8,18 @@ eg.RegisterPlugin(
 )
 
 import time
+import os
+import ctypes
+from ctypes import byref, c_char_p, Structure, POINTER
+from ctypes.wintypes import DWORD
+import win32file
+import win32con
 import wx
 import wx.lib.masked as masked
+
+FT_OPEN_BY_DESCRIPTION = 2
+
+
 
 
 class Fhz1000Pc(eg.PluginClass):
@@ -27,18 +37,38 @@ class Fhz1000Pc(eg.PluginClass):
         
         
     def __start__(self):
+        self.timeTask = None
+        self.readBuffer = ""
+        
         global d2xx
-        import d2xx
-        self.handle = d2xx.openEx('ELV FHZ 1000 PC', d2xx.OPEN_BY_DESCRIPTION)
-        self.handle.setBaudRate(9600)
-        self.handle.setDataCharacteristics(8, 0, 0)
-        self.handle.setFlowControl(0, 17, 19)
-        # set RX/TX timeouts
-        self.handle.setTimeouts(1000,1000)
+        d2xx = ctypes.windll.LoadLibrary("ftd2xx.dll")
+        self.ftHandle = d2xx.FT_W32_CreateFile(
+            'ELV FHZ 1000 PC',
+            win32con.GENERIC_READ|win32con.GENERIC_WRITE,
+            0, # exclusive access
+            0, # no security
+            win32con.OPEN_EXISTING,
+            win32con.FILE_ATTRIBUTE_NORMAL|win32con.FILE_FLAG_OVERLAPPED|FT_OPEN_BY_DESCRIPTION,
+            0
+        )
+        self.receiveThread = eg.SerialThread(self.ftHandle)
+        self.receiveThread.WriteFile = d2xx.FT_W32_WriteFile
+        self.receiveThread.ReadFile = d2xx.FT_W32_ReadFile
+        self.receiveThread.ClearCommError = d2xx.FT_W32_ClearCommError
+        
+        d2xx.FT_SetLatencyTimer(self.ftHandle, 2)
+        d2xx.FT_SetBaudRate(self.ftHandle, 9600)
+        d2xx.FT_SetDataCharacteristics(self.ftHandle, 8, 0, 0)
+        d2xx.FT_SetFlowControl(self.ftHandle, 0, 17, 19)
+        d2xx.FT_SetTimeouts(self.ftHandle, 1000, 1000)
+        
+        self.receiveThread.SuspendReadEvents()
+        self.receiveThread.SetReadEventCallback(self.HandleReceive)
+        self.receiveThread.Start()
         # Say hello
         self.WriteFhz(0xC9, 0x02, 0x01, 0x1f, 0x42)
         self.ReadFhz()
-        
+
         # Request Status/Serial
         self.WriteFhz(0x04, 0xc9, 0x01, 0x84, 0x57, 0x02, 0x08)
         self.ReadFhz()
@@ -49,56 +79,116 @@ class Fhz1000Pc(eg.PluginClass):
         # FS20 Init (if required)
         self.WriteFhz(0x04, 0xc9, 0x01, 0x96)
         
-        self.SetFhzTime()
+        # calculate the time of the current minute
+        t = list(time.localtime())
+        t[5] = 0
+        self.nextTaskTime = time.mktime(t)
+        self.WriteFhz(*self.GetTimeData())
+        self.nextTaskTime += 60.0
+#        self.timeTask = eg.scheduler.AddTaskAbsolute(
+#            self.nextTaskTime, 
+#            self.TimeScheduleTask
+#        )
+#        commTimeouts = FTTIMEOUTS()
+#        commTimeouts.ReadIntervalTimeout = 0
+#        commTimeouts.ReadTotalTimeoutMultiplier = 0
+#        commTimeouts.ReadTotalTimeoutConstant = 10
+#        commTimeouts.WriteTotalTimeoutMultiplier = 0
+#        commTimeouts.WriteTotalTimeoutConstant = 200    
+#        d2xx.FT_W32_SetCommTimeouts(self.ftHandle, byref(commTimeouts))
+        self.receiveThread.ResumeReadEvents()
+        
         
         
     def __stop__(self):
+        if self.timeTask is not None:
+            eg.scheduler.CancelTask(self.timeTask)
         self.WriteFhz(0x04, 0xc9, 0x01, 0x97)
-        self.handle.close()
-        self.handle = None
+        if self.ftHandle:
+            d2xx.FT_W32_CloseHandle(self.ftHandle)
+            self.ftHandle = None
+        if self.receiveThread:
+            self.receiveThread.Stop()
         
         
-    def WriteFhz(self, telegramType, *args):
-        maxTime = time.clock() + 1.0
-        while self.handle.getModemStatus() != 48:
-            if time.clock() > maxTime:
-                self.PrintError("FHZ timeout error!")
-                return
-            time.sleep(0.01)
+    def HandleReceive(self, serial):
+        data = serial.Read(512)
+        print "HR: " + " ".join(["%02X" % ord(c) for c in data])
+        
+        
+    def WriteFhzNoWait(self, telegramType, *args):
         crc = 0xff & sum(args)
         dataStr = "".join([chr(x) for x in args])
         data = "\x81" + chr(len(dataStr) + 2) + chr(telegramType) + chr(crc) + dataStr
         if eg.debugLevel:
-            print "<- " + " ".join(["%02X" % ord(c) for c in data])
-        self.handle.write(data)
+            print "W: " + " ".join(["%02X" % ord(c) for c in data])
+        self.receiveThread.Write(data)
+        time.sleep(0.01)
+
+        
+    def WriteFhz(self, telegramType, *args):
+        maxTime = time.clock() + 1.0
+        dwStatus = DWORD()
+        while True:
+            d2xx.FT_GetModemStatus(self.ftHandle, byref(dwStatus))
+            if dwStatus.value == 48:
+                break
+            if time.clock() > maxTime:
+                self.PrintError("FHZ timeout error!")
+                return
+            time.sleep(0.01)
+            #print "write sleep"
+        self.WriteFhzNoWait(telegramType, *args)
         
         
     def ReadFhz(self):
-        time.sleep(0.2)
-        h = self.handle
-        startByte = ord(h.read(1))
+        startByte = ord(self.Read(1))
         if startByte != 0x81:
             raise FhzException("Wrong start byte.")
-        length = ord(h.read(1))
-        data = [ord(c) for c in h.read(length)]
+        length = ord(self.Read(1))
+        data = [ord(c) for c in self.Read(length)]
         telegramType = data[0]
         crc = data[1]
         newCrc = 0xff & sum(data[2:])
-        dataStr = " ".join(["%02X" % x for x in data])
         if eg.debugLevel:
+            dataStr = " ".join(["%02X" % x for x in data])
             print ("-> %02X %02X " % (startByte, length)) + dataStr
         return telegramType, data[2:]
     
     
-    def SetFhzTime(self):
-        t = time.localtime()
-        year = t.tm_year % 100 
-        self.WriteFhz(
+    def Read(self, numBytes):
+        data = self.receiveThread.Read(numBytes, 1.0)
+        if len(data) < numBytes:
+            self.PrintError("FHZ read timeout error!")
+        return data
+        
+    
+        
+    def GetTimeData(self):
+        t_struct = time.localtime(self.nextTaskTime)
+        year = t_struct.tm_year % 100 
+        return(
             0xc9, 0x02, 0x01, 0x61, 
-            year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min
+            year, t_struct.tm_mon, t_struct.tm_mday, 
+            t_struct.tm_hour, t_struct.tm_min
         )
-    
-    
+        
+        
+    def TimeScheduleTask(self, repeats=32):
+        """
+        Send the current time 50 times and schedule the next execution at the
+        next minute.
+        """
+        data = self.GetTimeData()
+        for i in range(repeats):
+            eg.actionThread.CallWait(self.WriteFhzNoWait, *data)
+        self.nextTaskTime += 60.0
+        self.timeTask = eg.scheduler.AddTaskAbsolute(
+            self.nextTaskTime, 
+            self.TimeScheduleTask
+        )
+
+        
     
 class ActionBase(eg.ActionClass):
     defaultAddress = 0x094001
