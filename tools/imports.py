f588f0f757bc6fc3ea0de0d4f6f6bcc7bfc42e89
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2008-06-30 16:54:42 +0000

    Created installer for 0.3.6.1445

--- tools/imports.py
+++ tools/imports.py
@@ -780,7 +780,6 @@ import win32ras
 import win32security
 import win32service
 import win32trace
-import win32transaction
 import win32ts
 import win32wnet
 import winxpgui
@@ -793,7 +792,6 @@ import netbios
 import ntsecuritycon
 import pywintypes
 import rasutil
-import regcheck
 import regutil
 import sspi
 import sspicon
@@ -849,9 +847,7 @@ import win32com.client.selecttlb
 import win32com.client.tlbrowse
 import win32com.client.util
 import win32com.makegw
-import win32com.makegw.makegw
 import win32com.makegw.makegwenum
-import win32com.makegw.makegwparse
 import win32com.server
 import win32com.server.connect
 import win32com.server.dispatcher
@@ -874,8 +870,6 @@ import win32com.authorization
 import win32com.authorization.authorization
 import win32com.axcontrol
 import win32com.axcontrol.axcontrol
-import win32com.bits
-import win32com.bits.bits
 import win32com.directsound
 import win32com.directsound.directsound
 import win32com.ifilter
@@ -888,8 +882,6 @@ import win32com.mapi.emsabtags
 import win32com.mapi.mapi
 import win32com.mapi.mapitags
 import win32com.mapi.mapiutil
-import win32com.propsys
-import win32com.propsys.pscon
 import win32com.shell
 import win32com.shell.shell
 import win32com.shell.shellcon
