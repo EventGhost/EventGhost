9f1a53cc4b81647711f32cf85e44794d2f1c0721
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2008-07-17 20:03:21 +0000

    Created installer for 0.3.6.1449

--- eg/Classes/Version.py
+++ eg/Classes/Version.py
@@ -26,7 +26,7 @@ class Version:
     minor = 3
     micro = 6
     releaselevel = 'beta'
-    buildNum = 1448
-    buildTime = 1215725361.55
+    buildNum = 1449
+    buildTime = 1216324921.17
     svnRevision = int('$LastChangedRevision$'.split()[1])
     string = "%s.%s.%s.%s" % (major, minor, micro, buildNum)
