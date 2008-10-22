94e6769fb8963cc2068e92419cd6a298d580ceb9
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2008-10-22 14:41:45 +0000

    Added authors name to eg.RegisterPlugin().

--- plugins/EventGhost/__init__.py
+++ plugins/EventGhost/__init__.py
@@ -23,6 +23,7 @@
 
 eg.RegisterPlugin(
     name = "EventGhost",
+    author = "Bitmonster",
     description = (
         "Here you find actions that mainly control the core "
         "functionality of EventGhost."
