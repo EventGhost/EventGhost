fd4cc1702eb9b641af7633c43db1d5955c523ad9
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2008-12-03 21:00:36 +0000

    Code cleanup and added "Start" action.
    git-svn-id: https://eventghost.svn.sourceforge.net/svnroot/eventghost/trunk@599 18fe9db4-152b-49c1-b228-172182a50ab1

--- plugins/VLC/__init__.py
+++ plugins/VLC/__init__.py
@@ -1,9 +1,9 @@
-# ==============================================================================
+# ============================================================================
 # VideoLan Client TCP/IP Interface v.0.3
 # Example VLC parameters:
 # Path     -Skin     --RC Interface --Adress                 --NoDosBox
 # vlc.exe  -I skins2 --extraintf=rc --rc-host=localhost:1234 --rc-quiet
-# ==============================================================================
+# ============================================================================
 #
 # Plugins/VLC/__init__.py
 #
@@ -25,11 +25,12 @@
 # along with EventGhost; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 
+import eg
 
 eg.RegisterPlugin(
     name = "VLC media player",
     author = "MonsterMagnet",
-    version = "0.3." + "$LastChangedRevision$".split()[1],
+    version = "0.4." + "$LastChangedRevision$".split()[1],
     kind = "program",
     canMultiLoad = True,
     createMacrosOnAdd = True,
@@ -65,44 +66,181 @@ eg.RegisterPlugin(
     url = "http://www.eventghost.org/forum/viewtopic.php?t=693",
 )
 
-import asynchat, socket
+import wx
+import os
+import asynchat
+import socket
+import _winreg
+from win32api import ShellExecute
 
-fnList = (
-('Play', 'Start playing', 'pause'),
-('Pause', 'Toggle play/pause', 'pause'),
-('Stop', 'Stop and close current item', 'stop'),
-('Fast Forward', 'Skip ~5 sec forward', 'fastforward'),
-('Fast Rewind', 'Skip ~5 sec back', 'rewind'),
-('Play Faster', 'Play faster', 'faster'),
-('Play Slower', 'Play slower', 'slower'),
-('Play Normal', 'Play normal', 'normal'),
-('Fullscreen', 'Toggle fullscreen', 'f'),
-('Next Playlist Item', 'Jump forward to the next item in playlist', 'next'),
-('Previous Playlist Item', 'Jump backward to the previous playlist item',
-    'prev'),
-('Next Title', 'Next title in current item', 'title_n'),
-('Previous Title', 'Previous title in current item', 'title_p'),
-('Next Chapter', 'Next chapter in current item', 'chapter_n'),
-('Previous Chapter', 'Previous chapter in current item', 'chapter_p'),
-('Current Playlist Status', 'If VLC feedback is enabled in plugin settings, '
-    'the logger shows information about the current playlist status.', 'status'),
-('Stream Info', 'If VLC feedback is enabled in plugin settings, the logger '
-    'shows information about the current stream.', 'info'),
-('Show Playlist', 'If VLC feedback is enabled in plugin settings, the logger '
-    'shows information about the current playlist.', 'playlist'),
-('Clear Playlist', 'Clear the playlist and close current item', 'clear'),
-('Volume Up', 'Volume up', 'volup'),
-('Volume Down', 'Volume down', 'voldown'),
-('Quit', 'Quit VLC', 'quit'),
-('Help', 'If VLC feedback is enabled in plugin settings, the logger shows '
-    'all available commands, use <i>"MyCommand"</i> to execute them.', 'H'),
+
+ACTIONS = (
+    (
+        'Play', 
+        'Play', 
+        'Start playing', 
+        'pause'
+    ),
+    (
+        'Pause', 
+        'Pause', 
+        'Toggle play/pause', 
+        'pause'
+    ),
+    (
+        'Stop', 
+        'Stop', 
+        'Stop and close current item', 
+        'stop'
+    ),
+    (
+        'FastForward', 
+        'Fast Forward', 
+        'Skip ~5 sec forward', 
+        'fastforward'
+    ),
+    (
+        'FastRewind', 
+        'Fast Rewind', 
+        'Skip ~5 sec back', 
+        'rewind'
+    ),
+    (
+        'PlayFaster', 
+        'Play Faster', 
+        'Play faster', 
+        'faster'
+    ),
+    (
+        'PlaySlower', 
+        'Play Slower', 
+        'Play slower', 
+        'slower'
+    ),
+    (
+        'PlayNormal', 
+        'Play Normal', 
+        'Play normal', 
+        'normal'
+    ),
+    (
+        'Fullscreen', 
+        'Fullscreen', 
+        'Toggle fullscreen', 
+        'f'
+    ),
+    (
+        'NextPlaylistItem', 
+        'Next Playlist Item', 
+        'Jump forward to the next item in playlist', 
+        'next'
+    ),
+    (
+        'PreviousPlaylistItem', 
+        'Previous Playlist Item', 
+        'Jump backward to the previous playlist item',
+        'prev'
+    ),
+    (
+        'NextTitle', 
+        'Next Title', 
+        'Next title in current item', 
+        'title_n'
+    ),
+    (
+        'PreviousTitle', 
+        'Previous Title', 
+        'Previous title in current item', 
+        'title_p'
+    ),
+    (
+        'NextChapter', 
+        'Next Chapter', 
+        'Next chapter in current item', 
+        'chapter_n'
+    ),
+    (
+        'PreviousChapter', 
+        'Previous Chapter', 
+        'Previous chapter in current item', 
+        'chapter_p'
+    ),
+    (
+        'CurrentPlaylistStatus', 
+        'Current Playlist Status', 
+        'If VLC feedback is enabled in plugin settings, the logger shows '
+            'information about the current playlist status.', 
+        'status'
+    ),
+    (
+        'StreamInfo', 
+        'Stream Info', 
+        'If VLC feedback is enabled in plugin settings, the logger shows '
+            'information about the current stream.', 
+        'info'
+    ),
+    (
+        'ShowPlaylist', 
+        'Show Playlist', 
+        'If VLC feedback is enabled in plugin settings, the logger shows '
+            'information about the current playlist.', 
+        'playlist'
+    ),
+    (
+        'ClearPlaylist', 
+        'Clear Playlist', 
+        'Clear the playlist and close current item', 
+        'clear'
+    ),
+    (
+        'VolumeUp', 
+        'Volume Up', 
+        'Volume up', 
+        'volup'
+    ),
+    (
+        'VolumeDown', 
+        'Volume Down', 
+        'Volume down', 
+        'voldown'
+    ),
+    (
+        'Quit', 
+        'Quit', 
+        'Quit VLC', 
+        'quit'
+    ),
+    (
+        'Help', 
+        'Help', 
+        'If VLC feedback is enabled in plugin settings, the logger shows all '
+            'available commands, use <i>"My Command"</i> to execute them.', 
+        'H'
+    ),
 )
 
 
+def GetVlcPath():
+    """
+    Tries to get the path of VLC's executable through querying the Windows 
+    registry.
+    """
+    try:
+        return _winreg.QueryValue(
+            _winreg.HKEY_LOCAL_MACHINE, 
+            "Software\\VideoLAN\\VLC"
+        )
+    except WindowsError:
+        return os.path.join(
+            eg.folderPath.ProgramFiles, 
+            "VideoLAN\\VLC\\vlc.exe"
+        )
 
-class VLC_Session(asynchat.async_chat):
+        
+        
+class VlcSession(asynchat.async_chat):
    
-    def __init__ (self, handler, address):
+    def __init__ (self, plugin, address):
         # Call constructor of the parent class
         asynchat.async_chat.__init__(self)
 
@@ -110,8 +248,8 @@ class VLC_Session(asynchat.async_chat):
         self.set_terminator('\r\n')
 
         # Initialize input data buffer
-        self.buffer = ''
-        self.handler = handler
+        self.data = ''
+        self.plugin = plugin
         self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
         eg.RestartAsyncore()
         try:
@@ -122,108 +260,197 @@ class VLC_Session(asynchat.async_chat):
 
     def handle_connect(self):
         # connection succeeded
-        self.handler.TriggerEvent("Connected")
-        self.sendall(self.handler.connectedevent)
+        self.plugin.TriggerEvent("Connected")
+        self.sendall(self.plugin.connectedevent)
          
         
     def handle_expt(self):
         # connection failed
-        self.handler.dispatcher_running = False
-        self.handler.TriggerEvent("NoConnection")
+        self.plugin.isDispatcherRunning = False
+        self.plugin.TriggerEvent("NoConnection")
         self.close()
 
 
     def handle_close(self):
         # connection closed
-        self.handler.dispatcher_running = False
-        self.handler.TriggerEvent("ConnectionLost")
+        self.plugin.isDispatcherRunning = False
+        self.plugin.TriggerEvent("ConnectionLost")
         self.close()
 
 
     def collect_incoming_data(self, data):
         # received a chunk of incoming data
-        self.buffer = self.buffer + data
+        self.data = self.data + data
 
 
     def found_terminator(self):
-        self.handler.ValueUpdate(self.buffer)
-        self.buffer = ''
+        self.plugin.ValueUpdate(self.data)
+        self.data = ''
          
+         
+         
+class ActionPrototype(eg.ActionClass):
+    value = None # the actual value will be defined through AddActionsFromList
+    
+    def __call__(self):
+        return self.plugin.Push(self.value + "\r\n")
+
+
+
+class Start(eg.ActionClass):
+    description = "Starts VLC with the needed command line arguments."
+    
+    class text:
+        additionalArgs = "Additional command line arguments:"
+        resultingCmdLine = "Resulting command line:"
         
-       
+        
+    def __call__(self, cmdLineArgs=""):
+        vlcPath = GetVlcPath()
+        return ShellExecute(
+            0, 
+            None, 
+            vlcPath, 
+            self.GetCmdLineArgs(cmdLineArgs), 
+            None, #os.path.dirname(vlcPath), 
+            1
+        )
+    
+    
+    def GetCmdLineArgs(self, cmdLineArgs):
+        args = '--extraintf=rc --rc-host=%s:%d --rc-quiet --rc-show-pos' % (
+            self.plugin.host, 
+            self.plugin.port
+        )
+        if cmdLineArgs:
+            args = args + " " + cmdLineArgs
+        return args
+        
+        
+    def GetLabel(self, cmdLineArgs=""):
+        if cmdLineArgs:
+            return self.name + ": " + cmdLineArgs
+        else:
+            return self.name
+        
+        
+    def Configure(self, cmdLineArgs=""):
+        vlcPath = GetVlcPath()
+        panel = eg.ConfigPanel(self, resizeable=True)
+        cmdLineCtrl = panel.TextCtrl(cmdLineArgs)
+        resultCtrl = eg.StaticTextBox(panel)
+        def OnTextChange(event=eg.wxDummyEvent):
+            cmdLineArgs = cmdLineCtrl.GetValue()
+            cmdString = '"%s" %s' % (vlcPath, self.GetCmdLineArgs(cmdLineArgs))
+            resultCtrl.SetValue(cmdString)
+            event.Skip()
+        OnTextChange()
+        cmdLineCtrl.Bind(wx.EVT_TEXT, OnTextChange) 
+
+        panel.sizer.AddMany([
+            (panel.StaticText(self.text.additionalArgs), 0, wx.BOTTOM, 3),
+            (cmdLineCtrl, 0, wx.EXPAND|wx.BOTTOM, 5),
+            ((15, 15), ),
+            (panel.StaticText(self.text.resultingCmdLine), 0, wx.BOTTOM, 3),
+            (resultCtrl, 1, wx.EXPAND),
+        ])
+        while panel.Affirmed():
+            panel.SetResult(cmdLineCtrl.GetValue())
+            
+            
+            
+class MyCommand(eg.ActionClass):
+    name = "My Command"
+    description = (
+        "Here you can enter your own command, ie. show a custom text message."
+    )
+    class text:
+        label = (
+            "My Command: (Type 'H' to see a list of all available commands.)"
+        )
+        
+    def __call__(self, text):
+        self.plugin.Push(eg.ParseString(text) + "\r\n")
+               
+            
+    def Configure(self, text="marq-marquee EventGhost"):
+        panel = eg.ConfigPanel(self)
+        mySizer = wx.FlexGridSizer(rows=3)
+        staticText = panel.StaticText(self.text.label)
+        textCtrl = panel.TextCtrl(text)
+        mySizer.Add(staticText, 0, wx.EXPAND|wx.ALL, 5)
+        mySizer.Add(textCtrl, 0, wx.EXPAND|wx.ALL, 5)
+        panel.sizer.Add(mySizer)
+        while panel.Affirmed():
+            panel.SetResult(textCtrl.GetValue())
+
+
+
 class VLC(eg.PluginClass):
-   
+    
+    class text:
+        eventBox = "Event generation"
+        showFeedbackEvents = "Show VLC feedback events"
+        tcpBox = "TCP/IP Settings"
+        host = "Host:"
+        port = "Port:"
+        
+        
     def __init__(self):
-        self.host = "localhost"
-        self.port = 1234
-        self.dispatcher_running = False
-        self.feedback = ""
-       
-        self.AddAction(self.MyCommand)
-       
-        for actionName, actionDescription, command, in fnList:
-            class tmpAction(eg.ActionClass):
-                name = actionName
-                description = actionDescription
-                value = command  + "\r\n"
-                def __call__(self2):
-                    return self.push(self2.value)
-                
-            tmpAction.__name__ = actionName.replace(" ", "")
-            self.AddAction(tmpAction)
+        self.AddAction(Start)
+        self.AddAction(MyCommand)
+        self.AddActionsFromList(ACTIONS, ActionPrototype)
                              
                             
-    def __start__(self, host="localhost", port=1234, feedback_events=True):                             
+    def __start__(self, host="localhost", port=1234, showFeedbackEvents=True): 
         self.host = host
         self.port = port
-        self.feedback_events = feedback_events
+        self.dispatcher = None
+        self.isDispatcherRunning = False
+        self.feedback = ""
+        self.showFeedbackEvents = showFeedbackEvents
        
     
     def __stop__(self):
-        pass
+        if self.isDispatcherRunning:
+            self.dispatcher.close()
 
-
-    def __close__(self):
-        pass
-   
 
     def ValueUpdate(self, text):
         state = text.decode('utf-8')
-        if self.feedback_events:
+        if self.showFeedbackEvents:
             self.TriggerEvent(state)
 
 
-    def push(self, data):
-        if not self.dispatcher_running:
+    def Push(self, data):
+        if not self.isDispatcherRunning:
             self.connectedevent = data
-            self.dispatcher = VLC_Session(self, (self.host, self.port))
-            self.dispatcher_running = True
+            self.dispatcher = VlcSession(self, (self.host, self.port))
+            self.isDispatcherRunning = True
         try:
             if self.dispatcher.connected:
                 self.dispatcher.sendall(data)
             return True
         except:
-            self.dispatcher_running = False
+            self.isDispatcherRunning = False
             self.PrintError("Error sending data to VLC.")
             self.dispatcher.close()
             return False
 
 
-    def Configure(self, host="localhost", port=1234, feedback_events=True): 
+    def Configure(self, host="localhost", port=1234, showFeedbackEvents=True): 
+        text = self.text
         panel = eg.ConfigPanel(self)
         hostCtrl = panel.TextCtrl(host)
         portCtrl = panel.SpinIntCtrl(port)
-        checkBox = panel.CheckBox(feedback_events, "Show VLC feedback events")
+        checkBox = panel.CheckBox(showFeedbackEvents, text.showFeedbackEvents)
         tcpBox = panel.BoxedGroup(
-            "TCP/IP Settings",
-            ("Host:", hostCtrl),
-            ("Port:", portCtrl),
+            text.tcpBox,
+            (text.host, hostCtrl),
+            (text.port, portCtrl),
         )
         eg.EqualizeWidths(tcpBox.GetColumnItems(0))
-        eventBox = panel.BoxedGroup(
-            "Event generation",
-            checkBox,
-        )
+        eventBox = panel.BoxedGroup(text.eventBox, checkBox)
         panel.sizer.Add(tcpBox, 0, wx.EXPAND)
         panel.sizer.Add(eventBox, 0, wx.TOP|wx.EXPAND, 10)
         while panel.Affirmed():
@@ -233,31 +460,4 @@ class VLC(eg.PluginClass):
                 checkBox.GetValue()
             )
    
-
-
-    class MyCommand(eg.ActionClass):
-
-        description = ("Here you can enter your own command, ie. show a " 
-                       "custom text message.")
-
-        def __call__(self, text):
-            self.plugin.push(eg.ParseString(text) + "\r\n")
-                   
-                
-        def Configure(self, text="marq-marquee EventGhost"):
-            panel = eg.ConfigPanel(self)
-            mySizer = wx.FlexGridSizer(rows=3)
-
-            staticText = panel.StaticText(
-                "My Command: (Type 'H' to see a list of all available "
-                "commands.)"
-            )
-            textEdit = panel.TextCtrl(text)
-            mySizer.Add(staticText, 0, wx.EXPAND|wx.ALL, 5)
-            mySizer.Add(textEdit, 0, wx.EXPAND|wx.ALL, 5)
-
-            panel.sizer.Add(mySizer)
-           
-            while panel.Affirmed():
-                panel.SetResult(textEdit.GetValue())
-            
+               
