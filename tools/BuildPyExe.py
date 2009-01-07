33564ae59bd857e1a0e98da51f9e058776ca87e8
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-01-07 22:22:41 +0000

    Add manifest also to py.exe and pyw.exe

--- tools/BuildPyExe.py
+++ tools/BuildPyExe.py
@@ -36,6 +36,74 @@ PY_BASE_NAME = "py%s" % PYVERSION
 PYW_BASE_NAME = "pyw%s" % PYVERSION
 TEMP_DIR = tempfile.mkdtemp()
 
+# The manifest will be inserted as resource into the exe.  This
+# gives the controls the Windows XP appearance (if run on XP ;-)
+PY25_MANIFEST_TEMPLATE = '''
+<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <assemblyIdentity
+        version="5.0.0.0"
+        processorArchitecture="x86"
+        type="win32"
+    />
+    <dependency>
+        <dependentAssembly>
+            <assemblyIdentity
+                type="win32"
+                name="Microsoft.Windows.Common-Controls"
+                version="6.0.0.0"
+                processorArchitecture="X86"
+                publicKeyToken="6595b64144ccf1df"
+                language="*"
+            />
+        </dependentAssembly>
+    </dependency>
+</assembly>
+'''
+
+PY26_MANIFEST_TEMPLATE = '''
+<assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+    <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+        <security>
+            <requestedPrivileges>
+                <requestedExecutionLevel 
+                    level="asInvoker" 
+                    uiAccess="false"
+                />
+            </requestedPrivileges>
+        </security>
+    </trustInfo>
+    <dependency>
+        <dependentAssembly>
+            <assemblyIdentity 
+                type="win32" 
+                name="Microsoft.VC90.CRT" 
+                version="9.0.21022.8" 
+                processorArchitecture="x86" 
+                publicKeyToken="1fc8b3b9a1e18e3b"
+            />
+        </dependentAssembly>
+    </dependency>
+    <dependency>
+        <dependentAssembly>
+            <assemblyIdentity
+                type="win32"
+                name="Microsoft.Windows.Common-Controls"
+                version="6.0.0.0"
+                processorArchitecture="X86"
+                publicKeyToken="6595b64144ccf1df"
+                language="*"
+            />
+        </dependentAssembly>
+    </dependency>
+</assembly>
+'''
+
+if PYVERSION == "25":
+    MANIFEST = PY25_MANIFEST_TEMPLATE
+elif PYVERSION == "26":
+    MANIFEST = PY26_MANIFEST_TEMPLATE
+    
 sys.argv.append("py2exe")
 setup(
     options=dict(
@@ -45,8 +113,20 @@ setup(
     # it is important, that the zipfile argument does match the one from
     # the main installer.
     zipfile="lib%s/python%s.zip" % (PYVERSION, PYVERSION),
-    windows=[dict(script="py.py", dest_base=PYW_BASE_NAME)],
-    console=[dict(script="py.py", dest_base=PY_BASE_NAME)],
+    windows=[
+        dict(
+            script="py.py", 
+            dest_base=PYW_BASE_NAME,
+            other_resources = [(24, 1, MANIFEST)],
+        )
+    ],
+    console=[
+        dict(
+            script="py.py", 
+            dest_base=PY_BASE_NAME,
+            other_resources = [(24, 1, MANIFEST)],
+        )
+    ],
     verbose=0,
 )
 OUT_DIR = abspath(join(dirname(sys.argv[0]), ".."))
