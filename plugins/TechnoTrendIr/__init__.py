cab0b3431b6e43474bb5a96c75bbfd5496730297
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-02-02 17:59:59 +0000

    Changed code to use the new eg.IrDecoder.

--- plugins/TechnoTrendIr/__init__.py
+++ plugins/TechnoTrendIr/__init__.py
@@ -20,43 +20,45 @@
 # $LastChangedRevision$
 # $LastChangedBy$
 
+"""<rst>
+Hardware plugin for the `TechnoTrend USB Infrared Receiver 
+<http://www.technotrend.com/2789/USB_Infrared_Receiver.html>`_.
+
+|
+
+.. image:: IR_receiver.jpg
+   :align: center
+   :target: http://www.technotrend.com/2789/USB_Infrared_Receiver.html
+"""
+
+
+import eg
 
 eg.RegisterPlugin(
-    name = "TechnoTrend USB-IR Receiver",
+    name = "TechnoTrend USB Infrared Receiver",
     author = "Bitmonster",
-    version = "1.0." + "$LastChangedRevision$".split()[1],
+    version = "1.1." + "$LastChangedRevision$".split()[1],
     kind = "remote",
-    description = (
-        'Hardware plugin for the '
-        '<a href="http://shop.technotrend.de/shop.php?mode=show_detail&group=6&id=545">'
-        'TechnoTrend USB-IR Receiver</a>.'
-        '\n\n<p>'
-        '<center><img src="IR_receiver.jpg" /></center>'
-    ),
+    description = __doc__,
 )
 
 import os
+from threading import Lock, Timer, Thread
+from time import clock, sleep
+from functools import partial
+
 from eg.WinApi.Dynamic import (
     cdll, DWORD, POINTER, ULONG, HANDLE, BYTE, c_void_p, c_int, CFUNCTYPE
 )
 
-# enum USBIR_MODES
-USBIR_MODE_NONE = 0
-USBIR_MODE_RAW = 1
 USBIR_MODE_DIV = 2
-USBIR_MODE_ALL_CODES = 3 # special mode for decting the type of a remote control
-USBIR_MODE_RC5 = 4
-USBIR_MODE_NOKIA = 5
-USBIR_MODE_NEC = 6
-USBIR_MODE_RCMM8 = 7 # USBIR_MODE_RCMM,
-USBIR_MODE_RC6 = 8 # RC6 Mode 6A
-USBIR_MODE_RECS80 = 9
-USBIR_MODE_DENON = 10 # = Sharp
-USBIR_MODE_MOTOROLA = 11
-USBIR_MODE_SONYSIRC = 12
 
-#    typedef void (*PIRCBFCN) (PVOID Context, PVOID Buf, ULONG len, // buffer length in bytes
-#                                USBIR_MODES IRMode, HANDLE hOpen, BYTE DevIdx);
+# typedef void (*PIRCBFCN) (PVOID Context, 
+#                           PVOID Buf, 
+#                           ULONG len, // buffer length in bytes
+#                           USBIR_MODES IRMode, 
+#                           HANDLE hOpen, 
+#                           BYTE DevIdx);
 IRCALLBACKFUNC = CFUNCTYPE(
     c_void_p, # return type
     c_void_p, # Context
@@ -68,28 +70,48 @@ IRCALLBACKFUNC = CFUNCTYPE(
 )
 
 
-class TTIR(eg.RawReceiverPlugin):
+class TTIR(eg.PluginBase):
+    
+    def __init__(self):
+        eg.PluginBase.__init__(self)
+        self.irDecoder = eg.IrDecoder(self, 1)
+
+
+    def __close__(self):
+        self.irDecoder.Close()
+    
     
     def __start__(self):
         self.dll = None
         self.hOpen = None
-        pluginDir = os.path.abspath(os.path.split(__file__)[0])
+        pluginDir = os.path.abspath(os.path.dirname(__file__))
         dll = cdll.LoadLibrary(os.path.join(pluginDir, "TTUSBIR.dll"))
         self.cCallback = IRCALLBACKFUNC(self.IrCallback)
-        self.hOpen = dll.irOpen(0, USBIR_MODE_ALL_CODES, self.cCallback, 0)
+        self.hOpen = dll.irOpen(0, USBIR_MODE_DIV, self.cCallback, 0)
         if self.hOpen == -1:
             raise self.Exceptions.DeviceNotFound
-        self.ir_GetUniqueCode = dll.ir_GetUniqueCode
-        self.ir_GetUniqueCode.restype  = DWORD
+        self.irGetUniqueCode = dll.ir_GetUniqueCode
+        self.irGetUniqueCode.restype  = DWORD
         self.dll = dll
-        
+        self.data = []
+        self.timer = eg.ResettableTimer(self.OnTimeout)
+        self.dataLock = Lock()
+        self.lastTime = clock()
+        self.startByte = 1
+        self.dll.irSetPowerLED(self.hOpen, 0)
+        self.ledTimer = eg.ResettableTimer(
+            partial(self.dll.irSetPowerLED, self.hOpen, 0)
+        )
+
     
-    def OnComputerSuspend(self, suspendType):
+    def OnComputerSuspend(self, dummySuspendType):
         self.dll.irClose(self.hOpen)
     
     
-    def OnComputerResume(self, suspendType):
-        self.hOpen = self.dll.irOpen(0, USBIR_MODE_ALL_CODES, self.cCallback, 0)
+    def OnComputerResume(self, dummySuspendType):
+        self.hOpen = self.dll.irOpen(
+            0, USBIR_MODE_DIV, self.cCallback, 0
+        )
         if self.hOpen == -1:
             raise self.Exceptions.DeviceNotFound
     
@@ -97,16 +119,35 @@ class TTIR(eg.RawReceiverPlugin):
     def __stop__(self):
         if self.dll is not None:
             self.dll.irClose(self.hOpen)
-    
+        self.timer.Stop()
+        self.ledTimer.Stop()
     
     def IrCallback(self, context, buf, length, irMode, hOpen, devIdx):
-        if irMode == USBIR_MODE_ALL_CODES: 
-            # special mode for decting the type of a remote control
-            for i in range(0, min(length // 8, 400)):
-                irMode2 = buf[i * 2]
-                code2 = buf[i * 2 + 1]
-                code = self.ir_GetUniqueCode(code2, irMode2)
-                self.TriggerEvent("%08X" % code)
+        if irMode == USBIR_MODE_DIV:
+            self.dll.irSetPowerLED(self.hOpen, 1)
+            self.ledTimer.Reset(1)
+            self.timer.Reset(80)
+            #self.lastTime = clock()
+            self.dataLock.acquire()
+            #print "---", length / 4, clock() - self.lastTime
+            append = self.data.append
+            for i in xrange(self.startByte, min(length/4, 500)):
+                value = buf[i] & 0x00ffffff
+                append(value)
+                if value > 12000:
+                    self.irDecoder.Decode(self.data, len(self.data))
+                    self.data = []
+                    append = self.data.append
+            self.startByte = 0
+            self.dataLock.release()
             
     
-    
+    def OnTimeout(self):
+        if self.dataLock.acquire(0):
+            data = self.data
+            self.data = []
+            self.startByte = 1
+            self.dataLock.release()
+            data.append(10000)
+            self.irDecoder.Decode(data, len(data))
+        
