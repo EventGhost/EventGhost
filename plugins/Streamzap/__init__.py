d828256b55753e7b6151f2fbab8f1ad63743e8a4
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-02-07 08:49:05 +0000

    Completely reworked the IR decoding.

--- plugins/Streamzap/__init__.py
+++ plugins/Streamzap/__init__.py
@@ -37,7 +37,7 @@ import eg
 eg.RegisterPlugin(
     name = "Streamzap PC Remote",
     author = "Bitmonster",
-    version = "1.0." + "$LastChangedRevision$".split()[1],
+    version = "1.1." + "$LastChangedRevision$".split()[1],
     kind = "remote",
     description = __doc__,
     icon = (
@@ -49,79 +49,113 @@ eg.RegisterPlugin(
 
 
 import threading
-import os
-import array
+from os.path import abspath, split, join
 from time import sleep
 from ctypes import cdll, c_ubyte, byref
 from ctypes.wintypes import DWORD
 
 
-SAMPLE_TIME = 0.000256
+DLL_PATH = join(abspath(split(__file__)[0]), "irdata.dll")
 
-MyDecoder = eg.IrDecoder(SAMPLE_TIME)
-MyDecoder.RC5_THRESHOLD = 5
-MyDecoder.suppressRc5ToggleBit = False
+CODES = {
+    0x0009C272: "OK",
+    0x00204272: "OK",
+    0x002709C2: "Left",
+    0x008109C2: "Left",
+    0x002709C8: "Right",
+    0x008109C8: "Right",
+    0x008109C0: "Up",
+    0x002709C0: "Up",
+    0x0020427C: "Down",
+    0x0009C27C: "Down",
+    0x02042402: "Num1",
+    0x009C2402: "Num1",
+    0x00270902: "Num2",
+    0x00810902: "Num2",
+    0x02042408: "Num3",
+    0x009C2408: "Num3",
+    0x0081090C: "Num4",
+    0x0027090C: "Num4",
+    0x0081090E: "Num5",
+    0x0027090E: "Num5",
+    0x00810908: "Num6",
+    0x00270908: "Num6",
+    0x02042420: "Num7",
+    0x009C2420: "Num7",
+    0x00810930: "Num8",
+    0x00270930: "Num8",
+    0x00810932: "Num9",
+    0x00270932: "Num9",
+    0x02042400: "Num0",
+    0x009C2400: "Num0",
+    0x0020424E: "Power",
+    0x0009C24E: "Power",
+    0x00810938: "Mute",
+    0x00270938: "Mute",
+    0x00810924: "ChanelUp",
+    0x00270924: "ChanelUp",
+    0x00810920: "ChanelDown",
+    0x00270920: "ChanelDown",
+    0x00810926: "VolumeUp",
+    0x00270926: "VolumeUp",
+    0x02042480: "VolumeDown",
+    0x009C2480: "VolumeDown",
+    0x0020427E: "Menu",
+    0x0009C27E: "Menu",
+    0x00204278: "Exit",
+    0x0009C278: "Exit",
+    0x008109E0: "Play",
+    0x002709E0: "Play",
+    0x00810990: "Pause",
+    0x00270990: "Pause",
+    0x00810992: "Stop",
+    0x00270992: "Stop",
+    0x00810992: "Stop",
+    0x00270992: "Stop",
+    0x0009C266: "PreviousTrack",
+    0x00204266: "PreviousTrack",
+    0x00270998: "NextTrack",
+    0x00810998: "NextTrack",
+    0x00270984: "Record",
+    0x00810984: "Record",
+    0x00270986: "Rewind",
+    0x00810986: "Rewind",
+    0x00270980: "Forward",
+    0x00810980: "Forward",
+    0x009C2100: "Red",
+    0x02042100: "Red",
+    0x009C2102: "Green",
+    0x02042102: "Green",
+    0x00810842: "Yellow",
+    0x00270842: "Yellow",
+    0x02042108: "Blue",
+    0x009C2108: "Blue",
+}
 
-pluginDir = os.path.abspath(os.path.split(__file__)[0])
-dllPath = os.path.join(pluginDir, "irdata.dll")
+# Even though the Streamzap should be able to deliver raw timecode diagrams,
+# it doesn't work right. Sometimes the last pulse is missing. So we use a
+# simple decoder especially for this remote, that only reacts to known
+# Streamzap codes.
+
+def Decode(data):
+    buf = 2
+    for value in data:
+        if value < 5:
+            pass
+        elif value < 10:
+            buf |= 1
+        else:
+            return None
+        buf <<= 1
+    return buf
 
 
-EVENTS = (
-    ("Power", ("RC5_3465", "RC5_3C65")),
-    ("Mute", ("RC5_68CB", "RC5_78CB")),
-    ("Num1", ("RC5_68C1", "RC5_78C1")),
-    ("Num2", ("RC5_3461", "RC5_3C61")),
-    ("Num3", ("RC5_68C3", "RC5_78C3")),
-    ("Num4", ("RC5_3462", "RC5_3C62")),
-    ("Num5", ("RC5_68C5", "RC5_78C5")),
-    ("Num6", ("RC5_3463", "RC5_3C63")),
-    ("Num7", ("RC5_68C7", "RC5_78C7")),
-    ("Num8", ("RC5_3464", "RC5_3C64")),
-    ("Num9", ("RC5_68C9", "RC5_78C9")),
-    ("Num0", ("RC5_3460", "RC5_3C60")),
-    ("ChannelUp", ("RC5_3466", "RC5_3C66")),
-    ("ChannelDown", ("RC5_3467", "RC5_3C67")),
-    ("VolumeUp", ("RC5_68CD", "RC5_78CD")),
-    ("VolumeDown", ("RC5_68CF", "RC5_78CF")),
-    ("Up", ("RC5_3468", "RC5_3C68")),
-    ("Left", ("RC5_68D1", "RC5_78D1")),
-    ("Ok", ("RC5_3469", "RC5_3C69")),
-    ("Right", ("RC5_68D3", "RC5_78D3")),
-    ("Down", ("RC5_346A", "RC5_3C6A")),
-    ("Menu", ("RC5_68D5", "RC5_78D5")),
-    ("Exit", ("RC5_346B", "RC5_3C6B")),
-    ("Play", ("RC5_68D7", "RC5_78D7")),
-    ("Pause", ("RC5_346C", "RC5_3C6C")),
-    ("Stop", ("RC5_68D9", "RC5_78D9")),
-    ("PreviousTrack", ("RC5_346D", "RC5_3C6D")),
-    ("NextTrack", ("RC5_68DB", "RC5_78DB")),
-    ("Record", ("RC5_346E", "RC5_3C6E")),
-    ("Rewind", ("RC5_68DD", "RC5_78DD")),
-    ("Forward", ("RC5_346F", "RC5_3C6F")),
-    ("Red", ("RC5_3470", "RC5_3C70")),
-    ("Green", ("RC5_68E1", "RC5_78E1")),
-    ("Yellow", ("RC5_3471", "RC5_3C71")),
-    ("Blue", ("RC5_68E3", "RC5_78E3")),
-)
 
-eventList = []
-mapTable = {}
-for name, (code1, code2) in EVENTS:
-    mapTable[code1] = (name, 0.140, None)
-    mapTable[code2] = (name, 0.140, None)
-    eventList.append((name, None))
+class Streamzap(eg.PluginBase):
     
-
-
-class Streamzap(eg.RawReceiverPlugin):
-    
-    def __init__(self):
-        eg.RawReceiverPlugin.__init__(self)
-        self.mapTable = mapTable
-        self.RegisterEvents(eventList)
-
-
-    def __start__(self, comport=0):
+    def __start__(self):
+        self.timer = eg.ResettableTimer(self.OnTimeout)
+        self.lastCode = None
         self.abortThread = False
         self.lastException = None
         startupEvent = threading.Event()
@@ -137,14 +171,15 @@ class Streamzap(eg.RawReceiverPlugin):
 
     def __stop__(self):
         self.abortThread = True
+        self.timer.Stop()
         self.thread.join(5.0)
         
-    
+        
     def ReceiveThread(self, startupEvent):
         # This is the code executing in the new thread. 
         try:
-            dll = cdll.LoadLibrary(dllPath)
-        except:
+            dll = cdll.LoadLibrary(DLL_PATH)
+        except WindowsError:
             self.lastException = self.Exceptions.DriverNotFound
             startupEvent.set()
             return
@@ -158,30 +193,37 @@ class Streamzap(eg.RawReceiverPlugin):
         
         dwNumBytesRead = DWORD()
         byteIRdata = c_ubyte()
-        irData = array.array("B")
-        for i in range(0, 512):
-            irData.append(0)
+        irData = []
         i = 0
-        
-        startupEvent.set()
         szReadFile = dll.sz_ReadFile
-        Decode = MyDecoder.Decode
+        startupEvent.set()
         while not self.abortThread:
             szReadFile(byref(byteIRdata), byref(dwNumBytesRead))
             val = byteIRdata.value
 
             if dwNumBytesRead.value == 0:
-                sleep(0.01)
+                sleep(0.001)
             else:
-                if val > 150 or i == 511:
-                    event = Decode(irData, i)
-                    if event is not None:
-                        self.TriggerEvent(event)
+                i += 1
+                if val > 78 or i == 511:
+                    code = Decode(irData)
+                    eventname = CODES.get(code, None)
+                    if eventname is None:
+                        self.EndLastEvent()
+                        self.lastCode = None
+                    else:
+                        if code != self.lastCode:
+                            self.TriggerEnduringEvent(eventname)
+                            self.lastCode = code
+                        self.timer.Reset(140)
+                    irData = []
                     i = 0
-                else:
-                    irData[i] = val
-                    i += 1
-                    
+                    continue
+                irData.append(val)
+
         dll.sz_Close()
  
-
+ 
+    def OnTimeout(self):
+        self.EndLastEvent()
+        self.lastCode = None
