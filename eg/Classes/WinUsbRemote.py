b62019e0fca764edff1257d883b6c8d2d4850e1e
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-07-03 12:03:44 +0000

    - The callback now gets the data as a tuple instead of a list. - Added an WinUsbRemote.Open() function.
    git-svn-id: https://eventghost.svn.sourceforge.net/svnroot/eventghost/trunk@1074 18fe9db4-152b-49c1-b228-172182a50ab1

--- eg/Classes/WinUsbRemote.py
+++ eg/Classes/WinUsbRemote.py
@@ -1,16 +1,27 @@
+# This file is part of EventGhost.
+# Copyright (C) 2009 Lars-Peter Voss <bitmonster@eventghost.org>
+#
+# EventGhost is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# EventGhost is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with EventGhost; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+
 import sys
-import os
+from os.path import join
 
 import eg
-from eg.WinApi.Dynamic import (
-    cast,
-    POINTER,
-    c_ubyte,
-    WinDLL,
-    WM_USER,
-)
+from eg.WinApi.Dynamic import cast, c_ubyte, POINTER, WinDLL
 
-P_UBYTE = POINTER(c_ubyte)
+PUBYTE = POINTER(c_ubyte)
 
 
 class WinUsbRemote(object):
@@ -24,40 +35,46 @@ class WinUsbRemote(object):
         dataSize=1, 
         suppressRepeat=False
     ):
+        self.callback = callback
+        self.dataSize = dataSize
+        self.suppressRepeat = suppressRepeat
+        self.deviceInterfaceGuid = unicode(deviceInterfaceGuid)
         if self.dll is None:
             self.__class__.dll = WinDLL(
-                os.path.join(
+                join(
                     eg.MAIN_DIR, 
                     "lib%d%d" % sys.version_info[:2], 
                     "site-packages", 
                     "WinUsbWrapper.dll"
                 )
             )
-        
-        self.callback = callback
-        self.dataSize = dataSize
-        self.msgId = eg.messageReceiver.AddWmUserHandler(self.WindowProcedure)
-        self.threadId = self.dll.Start(
-            eg.messageReceiver.hwnd,
-            self.msgId,
-            unicode(deviceInterfaceGuid),
-            dataSize,
-            int(suppressRepeat)
-        )
+        self.Open()
         
     
     def IsOk(self):
         return bool(self.threadId)
     
     
+    def Open(self):
+        msgId = eg.messageReceiver.AddWmUserHandler(self.MsgHandler)
+        self.threadId = self.dll.Start(
+            eg.messageReceiver.hwnd,
+            msgId,
+            self.deviceInterfaceGuid,
+            self.dataSize,
+            int(self.suppressRepeat)
+        )
+        
+    
     def Close(self):
         self.dll.End(self.threadId)
-        eg.messageReceiver.RemoveWmUserHandler(self.WindowProcedure)
+        self.threadId = None
+        eg.messageReceiver.RemoveWmUserHandler(self.MsgHandler)
 
 
-    def WindowProcedure(self, hwnd, msg, wParam, lParam):
-        p = cast(lParam, P_UBYTE)
-        value = [p[i] for i in range(self.dataSize)]
+    def MsgHandler(self, dummyHwnd, dummyMsg, dummyWParam, lParam):
+        dataArray = cast(lParam, PUBYTE)
+        value = tuple(dataArray[i] for i in range(self.dataSize))
         self.callback(value)
         return 1
-        
+
