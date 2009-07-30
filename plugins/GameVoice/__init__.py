7c7ad1cd3691f1d40607b5afa96b970230a1a30b
Bartman <Bartman@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-07-30 19:18:30 +0000

    created base plugin classes for ELV FS20 PCE and Sidewinder GameVoice.
    git-svn-id: https://eventghost.svn.sourceforge.net/svnroot/eventghost/trunk@1118 18fe9db4-152b-49c1-b228-172182a50ab1

---
+++ plugins/GameVoice/__init__.py
@@ -0,0 +1,19 @@
+eg.RegisterPlugin(
+    name = "Sidewinder Game Voice",
+    author = "Bartman",
+    version = "0.1." + "$LastChangedRevision: 614 $".split()[1],
+    kind = "remote",
+    canMultiLoad = False,
+    description = (
+        'Allows the communication with the Microsoft Sidewinder Game Voice.<br/>'
+        'This plug in also demonstrates how to use EventGhost\'s HID API.'
+    ),
+    url = "http://www.eventghost.org/forum/viewtopic.php?t=571",
+)
+
+class GameVoice(eg.PluginClass):
+    def __start__(self):
+        pass
+
+    def __stop__(self):
+        pass        
