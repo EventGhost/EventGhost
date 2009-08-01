c60c25f94e18e809d0a721273b88e9b8477a79fc
Bartman <Bartman@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-08-01 21:57:35 +0000

    added specification readable event names
    git-svn-id: https://eventghost.svn.sourceforge.net/svnroot/eventghost/trunk@1130 18fe9db4-152b-49c1-b228-172182a50ab1

--- plugins/FS20PCE/__init__.py
+++ plugins/FS20PCE/__init__.py
@@ -20,9 +20,59 @@ from eg.WinApi.HID import DeviceDescription
 class Text:
     errorFind = "Error finding ELV FS20 PCE"
 
+Commands = {
+    0x00 : "Power.Off",
+    0x01 : "Power.Dim.1",
+    0x02 : "Power.Dim.2", 
+    0x03 : "Power.Dim.3",
+    0x04 : "Power.Dim.4",
+    0x05 : "Power.Dim.5",
+    0x06 : "Power.Dim.6",
+    0x07 : "Power.Dim.7",
+    0x08 : "Power.Dim.8",
+    0x09 : "Power.Dim.9",
+    0x10 : "Power.Dim.10",
+    0x11 : "Power.Dim.11",
+    0x12 : "Power.Dim.12",
+    0x13 : "Power.Dim.13",
+    0x14 : "Power.Dim.14",
+    0x15 : "Power.Dim.15",
+    0x16 : "Power.On",
+    0x17 : "Power.Previous",
+    0x18 : "Power.Toggle",
+    0x19 : "Power.Dim.Up",
+    0x20 : "Power.Dim.Down",
+    0x21 : "Power.Dim.UpAndDown",
+    0x22 : "Program.Time",
+    0x23 : "SendStatus",
+    0x24 : "Power.Timed.OffPreviousValue",
+    0x25 : "Power.Timed.OnOff",
+    0x26 : "Power.Timed.PreviousValueOff",
+    0x27 : "Program.Reset",
+    0x28 : "Program.DimUpTime",
+    0x29 : "Program.DimDownTime",
+    0x30 : "Power.Timed.OnPreviousState",
+    0x31 : "Power.Timed.PreviousValuePreviousState",
+}
+
 class FS20PCE(eg.PluginClass):
     def RawCallback(self, data):
-        self.TriggerEvent(binascii.hexlify(data).upper())
+        if not data or len(data) != 13 or ord(data[0]) != 2 or ord(data[1]) != 11:
+            self.PrintError("invalid data")
+            return
+        
+        #versionMajor = ord(data[12]) / 16
+        #versionMinor = ord(data[12]) % 16
+        
+        houseCode = binascii.hexlify(data[2:6])
+        deviceAddress = binascii.hexlify(data[6:8])
+        command = ord(data[8])
+        if command in Commands:
+            commandStr = Commands[command]
+        else:
+            commandStr = binascii.hexlify(data[8]).upper()
+
+        self.TriggerEvent(houseCode + "." + deviceAddress + "." + commandStr)
             
     def StopCallback(self):
         self.TriggerEvent("Stopped")
