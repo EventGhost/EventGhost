537d58ebcd65c1bdced42d26e01132dff388e5bc
Bartman <Bartman@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-08-01 20:01:05 +0000

    made raw events working

--- plugins/FS20PCE/__init__.py
+++ plugins/FS20PCE/__init__.py
@@ -11,9 +11,69 @@ eg.RegisterPlugin(
     url = "http://www.eventghost.org/forum/viewtopic.php?t=571",
 )
 
+import binascii
+from eg.WinApi.HID import HIDThread
+from eg.WinApi.HID import GetDevicePath
+from eg.WinApi.HID import GetDeviceDescriptions
+from eg.WinApi.HID import DeviceDescription
+
+class Text:
+    errorFind = "Error finding ELV FS20 PCE"
+
 class FS20PCE(eg.PluginClass):
+    def RawCallback(self, data):
+        self.TriggerEvent(binascii.hexlify(data).upper())
+            
+    def StopCallback(self):
+        self.TriggerEvent("Stopped")
+        self.thread = None
+
+    def GetMyDevicePath(self):
+        path = GetDevicePath(
+            None,
+            6383,
+            57364,
+            None,
+            0,
+            False)
+        return path;
+    
+    def SetupHidThread(self, newDevicePath):
+        #create thread
+        self.thread = HIDThread(self.name, newDevicePath)
+        self.thread.start()
+        self.thread.SetStopCallback(self.StopCallback)
+        self.thread.SetRawCallback(self.RawCallback)
+    
+    def ReconnectDevice(self, event):
+        """method to reconnect a disconnect device"""
+        if self.thread == None:
+            #updating device list
+            
+            #check if the right device was connected
+            #getting devicePath
+            newDevicePath = self.GetMyDevicePath()
+            if not newDevicePath:
+                #wrong device
+                return
+            
+            self.SetupHidThread(newDevicePath)
+
     def __start__(self):
-        pass
+        #Bind plug in to RegisterDeviceNotification message 
+        eg.Bind("System.DeviceAttached", self.ReconnectDevice)
+        
+        newDevicePath = self.GetMyDevicePath()
+        if not newDevicePath:
+            #device not found
+            self.PrintError(Text.errorFind)
+        else:
+            self.SetupHidThread(newDevicePath)
 
     def __stop__(self):
-        pass        
+        if self.thread:
+            self.thread.AbortThread()
+        
+        #unbind from RegisterDeviceNotification message
+        eg.Unbind("System.DeviceAttached", self.ReconnectDevice)
+   
