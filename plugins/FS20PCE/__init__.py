2788e53b254208438db69fcf7904dcc5bfe930e7
Bartman <Bartman@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-09-26 20:07:38 +0000

    added gui for groups
    git-svn-id: https://eventghost.svn.sourceforge.net/svnroot/eventghost/trunk@1219 18fe9db4-152b-49c1-b228-172182a50ab1

--- plugins/FS20PCE/__init__.py
+++ plugins/FS20PCE/__init__.py
@@ -13,12 +13,23 @@ eg.RegisterPlugin(
 
 import re
 import binascii
+import sys
+import wx.lib.mixins.listctrl as listmix
+from wx.lib.masked import TextCtrl
 from eg.WinApi.HID import HIDThread
 from eg.WinApi.HID import GetDevicePath
 from eg.WinApi.HID import IsDeviceName
 
 class Text:
     errorFind = "Error finding ELV FS20 PCE"
+    help0 = "You can assign a name to a combination of house code and device addresses."
+    help1 = "Use '?' as a wild card to define groups."
+    houseCode = "House code"
+    deviceAddress = "Device address"
+    groupName = "Group name"
+    add = "Add"
+    update = "Update"
+    delete = "Delete"
 
 VENDOR_ID = 6383
 PRODUCT_ID = 57364
@@ -230,7 +241,7 @@ class FS20PCE(eg.PluginClass):
             
             self.SetupHidThread(newDevicePath)
 
-    def __start__(self):
+    def __start__(self, mappings = []):
         #Bind plug in to RegisterDeviceNotification message 
         eg.Bind("System.DeviceAttached", self.ReconnectDevice)
         
@@ -247,4 +258,165 @@ class FS20PCE(eg.PluginClass):
         
         #unbind from RegisterDeviceNotification message
         eg.Unbind("System.DeviceAttached", self.ReconnectDevice)
+        
+    def Configure(self,
+        mappings = [],
+    ):
+        
+        #gui callbacks and helper methods
+        def ValidChars(input):
+            for char in input:
+                if char != '1' and char != '2' and char != '3' and char != '4' and char != '?': 
+                    return False
+            return True
+        
+        def ContainsItem(houseCode, deviceAddress, groupName):
+            for i in range(0, mappingsList.GetItemCount()):
+                if houseCode != mappingsList.GetItem(i, 0).GetText():
+                    continue
+                if deviceAddress != mappingsList.GetItem(i, 1).GetText():
+                    continue
+                if groupName != mappingsList.GetItem(i, 2).GetText():
+                    continue
+                return True
+            return False
+        
+        def OnItemSelected(event):
+            idx = mappingsList.GetFirstSelected()
+            houseCodeTextCtrl.SetValue(mappingsList.GetItem(idx, 0).GetText())
+            deviceAddressTextCtrl.SetValue(mappingsList.GetItem(idx, 1).GetText())
+            groupNameCtrl.SetValue(mappingsList.GetItem(idx, 2).GetText())
+            
+        def EnableButtons(event):
+            idx = mappingsList.GetFirstSelected()
+            houseCode = houseCodeTextCtrl.GetValue()
+            deviceAddress = deviceAddressTextCtrl.GetValue()
+            groupName = groupNameCtrl.GetValue()
+            valid = houseCodeTextCtrl.IsValid() and deviceAddressTextCtrl.IsValid() and len(groupName)
+            
+            itemSelected = idx != -1
+            addButton.Enable(valid and not ContainsItem(houseCode, deviceAddress, groupName))
+            updateButton.Enable(valid and itemSelected and \
+                (houseCode != mappingsList.GetItem(idx, 0).GetText() \
+                 or deviceAddress != mappingsList.GetItem(idx, 1).GetText() \
+                 or groupName != mappingsList.GetItem(idx, 2).GetText()) \
+                and not ContainsItem(houseCode, deviceAddress, groupName))
+            deleteButton.Enable(itemSelected)
+            event.Skip()
+        
+        def OnAddButton(event):
+            idx = mappingsList.InsertStringItem(sys.maxint, houseCodeTextCtrl.GetValue())
+            mappingsList.SetStringItem(idx, 1, deviceAddressTextCtrl.GetValue())
+            mappingsList.SetStringItem(idx, 2, groupNameCtrl.GetValue())
+            mappingsList.Select(idx)
+            EnableButtons(event)
+            event.Skip()
+        
+        def OnUpdateButton(event):
+            idx = mappingsList.GetFirstSelected()
+            mappingsList.SetStringItem(idx, 0, houseCodeTextCtrl.GetValue())
+            mappingsList.SetStringItem(idx, 1, deviceAddressTextCtrl.GetValue())
+            mappingsList.SetStringItem(idx, 2, groupNameCtrl.GetValue())
+            EnableButtons(event)
+            event.Skip()
+        
+        def OnDeleteButton(event):
+            idx = mappingsList.GetFirstSelected()
+            mappingsList.DeleteItem(idx)
+            EnableButtons(event)
+            event.Skip()
+
+        panel = eg.ConfigPanel(self, resizable=True)
+        
+        #building dialog
+        mappingsList = wx.ListCtrl(panel, -1, pos=wx.DefaultPosition,
+            size=wx.DefaultSize, style=wx.LC_REPORT | wx.LC_SINGLE_SEL)
+
+        #create list
+        mappingsList.InsertColumn(0, Text.houseCode)
+        mappingsList.InsertColumn(1, Text.deviceAddress)
+        mappingsList.InsertColumn(2, Text.groupName)
+        
+        #add items
+        if mappings:
+            for item in mappings:
+                idx = mappingsList.InsertStringItem(sys.maxint, item[0])
+                mappingsList.SetStringItem(idx, 1, item[1])
+                mappingsList.SetStringItem(idx, 2, item[2])
+
+        #layout
+        for i in range(mappingsList.GetColumnCount()):
+            mappingsList.SetColumnWidth(i, wx.LIST_AUTOSIZE_USEHEADER)
+            size = mappingsList.GetColumnWidth(i)
+            mappingsList.SetColumnWidth(i, wx.LIST_AUTOSIZE)
+            mappingsList.SetColumnWidth(i, max(size, mappingsList.GetColumnWidth(i) + 5))
+
+        houseCodeTextCtrl = TextCtrl( 
+            panel, 
+            mask = "XXXXXXXX",
+            choiceRequired = True,
+            validFunc = ValidChars,
+            defaultValue = "????????",
+            formatcodes = "F",
+        )
+        
+        deviceAddressTextCtrl = TextCtrl( 
+            panel, 
+            mask = "XXXX",
+            choiceRequired = True,
+            validFunc = ValidChars,
+            defaultValue = "????",
+            formatcodes = "F",
+        )
+        
+        groupNameCtrl = wx.TextCtrl(panel)
+        
+        editSizer = wx.GridSizer(3, 2)
+        editSizer.Add(wx.StaticText(panel, -1, Text.houseCode + ":"), wx.ALIGN_CENTER_VERTICAL)
+        editSizer.Add(houseCodeTextCtrl, 0)
+        editSizer.Add(wx.StaticText(panel, -1, Text.deviceAddress + ":"), wx.ALIGN_CENTER_VERTICAL)
+        editSizer.Add(deviceAddressTextCtrl, 0)
+        editSizer.Add(wx.StaticText(panel, -1, Text.groupName), wx.ALIGN_CENTER_VERTICAL)
+        editSizer.Add(groupNameCtrl, 0)
+
+        
+        addButton = wx.Button(panel, -1, Text.add)
+        updateButton = wx.Button(panel, -1, Text.update)
+        deleteButton = wx.Button(panel, -1, Text.delete)
+        
+        buttonsSizer = wx.GridSizer(1, 3)
+        buttonsSizer.Add(addButton)
+        buttonsSizer.Add(deleteButton)
+        buttonsSizer.Add(updateButton)
+        
+        houseCodeTextCtrl.Bind(wx.EVT_TEXT, EnableButtons)
+        deviceAddressTextCtrl.Bind(wx.EVT_TEXT, EnableButtons)
+        groupNameCtrl.Bind(wx.EVT_TEXT, EnableButtons);
+        
+        addButton.Bind(wx.EVT_BUTTON, OnAddButton)
+        updateButton.Bind(wx.EVT_BUTTON, OnUpdateButton)
+        deleteButton.Bind(wx.EVT_BUTTON, OnDeleteButton)
+        
+        mappingsList.Bind(wx.EVT_LIST_ITEM_SELECTED, OnItemSelected)
+        mappingsList.Bind(wx.EVT_LIST_ITEM_SELECTED, EnableButtons)
+        mappingsList.Bind(wx.EVT_LIST_ITEM_DESELECTED, EnableButtons)
+        EnableButtons(wx.CommandEvent())
+
+        panel.sizer.Add(wx.StaticText(panel, -1, Text.help0))
+        panel.sizer.Add(wx.StaticText(panel, -1, Text.help1))
+        panel.sizer.Add(mappingsList, 1, flag = wx.EXPAND)
+        panel.sizer.Add(editSizer)
+        panel.sizer.Add(buttonsSizer)
+
+        
+        while panel.Affirmed():
+            newMappings = []
+            for i in range(0, mappingsList.GetItemCount()):
+                item = mappingsList.GetItem(i, 0).GetText(), \
+                    mappingsList.GetItem(i, 1).GetText(), \
+                    mappingsList.GetItem(i, 2).GetText()
+                newMappings.append(item)
+            panel.SetResult(newMappings)
+
+
    
