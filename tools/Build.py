81d4fc1d88c2f0ad48213198c99cd5cf475d3398
Bitmonster <Bitmonster@18fe9db4-152b-49c1-b228-172182a50ab1>
2009-12-11 14:23:54 +0000

    Removed unused imports.
    git-svn-id: https://eventghost.svn.sourceforge.net/svnroot/eventghost/branches/bitmonster@1263 18fe9db4-152b-49c1-b228-172182a50ab1

--- tools/Build.py
+++ tools/Build.py
@@ -18,7 +18,6 @@
 This script creates the EventGhost setup installer.
 """
 import sys
-import codecs
 
 
 class StdErr(object):
@@ -36,10 +35,8 @@ class StdErr(object):
 sys.stderr = StdErr(sys.stderr, sys.stderr.encoding)
 
 import os
-import traceback
 from os.path import dirname, join, exists
 from glob import glob
-import threading
 
 # local imports
 import builder
