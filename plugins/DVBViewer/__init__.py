5074aaf5e4c67261330350e664bb8d1a7702e415
pako2 <pako2@18fe9db4-152b-49c1-b228-172182a50ab1>
2012-04-05 06:35:05 +0000

    DVBViewer plugin - Fixed Unicode problem in GetChannelDetails                   (channel names with special characters or umlauts caused an exception)
    git-svn-id: https://eventghost.svn.sourceforge.net/svnroot/eventghost/branches/pako2@1568 18fe9db4-152b-49c1-b228-172182a50ab1

--- plugins/DVBViewer/__init__.py
+++ plugins/DVBViewer/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 
-VERSION = "2.1.1"
+VERSION = "2.1.2"
 
 SUPPORTED_DVBVIEWER_VERSIONS        = '4.9.x (older versions might work but are untested)'
 SUPPORTED_DVBVIEWERSERVICE_VERSIONS = '1.9.x (older versions might work but are untested)'
@@ -24,6 +24,8 @@ SUPPORTED_DVBVIEWERSERVICE_VERSIONS = '1.9.x (older versions might work but are 
 # Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 #
 # Version history (newest on top):
+# 2.1.2: Fixed Unicode problem in GetChannelDetails; 
+#        channel names with special characters or umlauts caused an exception.
 # 2.1.1: Documentation and exception handling improved (minor changes)
 #        Return type of GetChannelDetails is now always a dictionary with the channelID as key.
 # 2.1.0: Added action GetTimerDetails
@@ -85,7 +87,6 @@ eg.RegisterPlugin(
         "YGVlk+dkZ+kiztQgv0IyleVOLNmEesH/3ZEAJPEK/gVNWWvcNcmGsgAAAABJRU5ErkJg"
         "gg=="
     ),
-    url = "http://www.eventghost.net/forum/viewtopic.php?f=9&t=1564",
 )
 
 
@@ -1092,10 +1093,10 @@ class DVBViewerWorkerThread(eg.ThreadWorker):
 
         if channelID != None:
             try:
-                channelID = str(channelID).split('|')[0]
+                channelID = unicode(channelID).split('|')[0]
                 result = { channelID: cpy(self.plugin.channelDetailsList[channelID]) }
             except:
-                eg.PrintError("No details for channel with ID '" + str(channelID) + "' found.")
+                eg.PrintError("No details for channel with ID '" + unicode(channelID) + "' found.")
                 result = None
             
         return result
