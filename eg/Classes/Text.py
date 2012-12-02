d0152ae3540028b2dd42dd7bfa2a87b4189c69da
pako2 <pako2@18fe9db4-152b-49c1-b228-172182a50ab1>
2012-12-02 11:17:25 +0000

    Some improvements (response to feature request) Developer tool: "Copy as Python": http://www.eventghost.net/forum/viewtopic.php?f=5&t=4014 "Rename Item" should work on events: http://www.eventghost.net/forum/viewtopic.php?f=5&t=4015

--- eg/Classes/Text.py
+++ eg/Classes/Text.py
@@ -120,6 +120,7 @@ class Default:
             Redo = "&Redo"
             Cut = "Cu&t"
             Copy = "&Copy"
+            Python = "Copy As Python"
             Paste = "&Paste"
             Delete = "&Delete"
             SelectAll = "Select &All"
