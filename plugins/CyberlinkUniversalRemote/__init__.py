f5262b5023d9ab34636c311a5efeae13e3cf4899
pako2 <pako2@18fe9db4-152b-49c1-b228-172182a50ab1>
2013-08-09 08:50:15 +0000

    Plugin CyberlinkUniversalRemote - Fix Power button
    git-svn-id: svn://svn.code.sf.net/p/eventghost/code/branches/pako2@1624 18fe9db4-152b-49c1-b228-172182a50ab1

--- plugins/CyberlinkUniversalRemote/__init__.py
+++ plugins/CyberlinkUniversalRemote/__init__.py
@@ -23,14 +23,14 @@ import eg
 eg.RegisterPlugin(
     name = "CyberLink Universal Remote Control",
     author = "Bitmonster",
-    version = "1.0.0",
+    version = "1.0.1",
     kind = "remote",
     guid = "{097D33BE-FD65-43D2-852B-5DA8A3FBC489}",
     description = __doc__,
     hardwareId = "USB\\VID_0766&PID_0204",
 )
 
-KEY_CODES = {
+KEY_CODES_1 = {
     (0, 0, 30, 0, 0, 0, 0, 0): "Num1",
     (0, 0, 31, 0, 0, 0, 0, 0): "Num2",
     (0, 0, 32, 0, 0, 0, 0, 0): "Num3",
@@ -47,38 +47,46 @@ KEY_CODES = {
     (0, 0, 80, 0, 0, 0, 0, 0): "Left",
     (0, 0, 81, 0, 0, 0, 0, 0): "Down",
     (0, 0, 82, 0, 0, 0, 0, 0): "Up",
-    (2, 2, 0, 0): "Power",
-    (4, 1, 0, 0): "Home",
-    (4, 128, 0, 0): "Red",
-    (4, 8, 0, 0): "Green",
-    (4, 16, 0, 0): "Yellow",
-    (4, 32, 0, 0): "Blue",
-    (4, 2, 0, 0): "TV",
-    (3, 128, 0, 0): "Record",
-    (3, 4, 0, 0): "Radio",
-    (4, 0, 16, 0): "SAP",
-    (4, 0, 32, 0): "Teletext",
-    (4, 0, 64, 0): "LastChannel",
-    (4, 0, 8, 0): "Subtitle",
-    (4, 0, 2, 0): "Language",
-    (4, 0, 1, 0): "Angel",
-    (3, 0, 4, 0): "Back",
-    (3, 0, 0, 2): "Info",
-    (4, 0, 4, 2): "DvdMenu",
-    (3, 0, 1, 0): "ChannelUp",
-    (3, 0, 2, 0): "ChannelDown",
-    (3, 16, 0, 0): "Mute",
-    (3, 32, 0, 0): "VolumeUp",
-    (3, 64, 0, 0): "VolumeDown",
-    (3, 0, 0, 64): "Play",
-    (3, 0, 0, 4): "Rewind",
-    (3, 0, 0, 128): "Pause",
-    (3, 0, 0, 8): "Forward",
-    (3, 2, 0, 0): "PreviousTrack",
-    (3, 0, 16, 0): "Stop",
-    (3, 1, 0, 0): "NextTrack",
 }
 
+KEY_CODES_2 = {
+    (3, 0, 0, 2):   "Info",
+    (3, 0, 0, 4):   "Rewind",
+    (3, 0, 0, 8):   "Forward",
+    (3, 0, 0, 64):  "Play",
+    (3, 0, 0, 128): "Pause",
+}
+
+KEY_CODES_3 = {
+    (3, 0, 1):   "ChannelUp",
+    (3, 0, 2):   "ChannelDown",
+    (3, 0, 4):   "Back",
+    (3, 0, 16):  "Stop",
+    (3, 1, 0):   "NextTrack",
+    (3, 2, 0):   "PreviousTrack",
+    (3, 4, 0):   "Radio",
+    (3, 16, 0):  "Mute",
+    (3, 32, 0):  "VolumeUp",
+    (3, 64, 0):  "VolumeDown",
+    (3, 128, 0): "Record",
+    (4, 0, 1):   "Angel",
+    (4, 0, 2):   "Language",
+    (4, 0, 4):   "DvdMenu",
+    (4, 0, 8):   "Subtitle",
+    (4, 0, 16):  "SAP",
+    (4, 0, 32):  "Teletext",
+    (4, 0, 64):  "LastChannel",
+    (4, 1, 0):   "Home",
+    (4, 2, 0):   "TV",
+    (4, 8, 0):   "Green",
+    (4, 16, 0):  "Yellow",
+    (4, 32, 0):  "Blue",
+    (4, 128, 0): "Red",
+}
+
+KEY_CODES_4 = {
+    (2, 2):   "Power",
+}
 
 class CyberlinkUniversalRemote(eg.PluginBase):
 
@@ -95,6 +103,7 @@ class CyberlinkUniversalRemote(eg.PluginBase):
             "USB\\VID_0766&PID_0204&MI_01"
         )
         self.winUsb.Start()
+        self.last_data = []
 
 
     def __stop__(self):
@@ -102,8 +111,22 @@ class CyberlinkUniversalRemote(eg.PluginBase):
 
 
     def Callback(self, data):
-        if data in KEY_CODES:
-            self.TriggerEnduringEvent(KEY_CODES[data])
-        else:
-            self.EndLastEvent()
+        if self.last_data != data:
+#            print data
+            if data in KEY_CODES_1:
+                self.TriggerEnduringEvent(KEY_CODES_1[data])
+                self.last_data = data
+            elif data in KEY_CODES_2:
+                self.TriggerEnduringEvent(KEY_CODES_2[data])
+                self.last_data = data
+            elif data[:3] in KEY_CODES_3:
+                self.TriggerEnduringEvent(KEY_CODES_3[data[:3]])
+                self.last_data = data
+            elif data[:2] in KEY_CODES_4:
+                self.TriggerEnduringEvent(KEY_CODES_4[data[:2]])
+                self.last_data = data
+            elif len(data) == len(self.last_data):
+                self.EndLastEvent()
+                self.last_data = []
+#                print "EndLastEvent"
 
