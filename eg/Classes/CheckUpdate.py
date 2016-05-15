d14f886adf558f14fc4b7b182e04c7a913d55230
topic2k <topic2k+git@gmail.com>
2016-05-15 13:56:32 +0200

    Correct the version compare.

--- eg/Classes/CheckUpdate.py
+++ eg/Classes/CheckUpdate.py
@@ -18,6 +18,7 @@
 
 import eg
 import wx
+import re
 import threading
 import webbrowser
 from agithub.GitHub import GitHub
@@ -37,7 +38,7 @@ class Text(eg.TranslatableStrings):
         "It wasn't possible to get the information from the EventGhost "\
         "website.\n\n"\
         "Please try it again later."
-
+    wipUpdateMsg = "Update check not available in developer version."
 
 
 class MessageDialog(eg.Dialog):
@@ -125,6 +126,9 @@ def _checkUpdate(manually=False):
     dialog = None
     try:
         if manually:
+            if eg.Version.base == "WIP":
+                wx.MessageBox(Text.wipUpdateMsg, eg.APP_NAME)
+                return
             dialog = ShowWaitDialog()
 
         gh = GitHub()
@@ -162,21 +166,22 @@ def _checkUpdate(manually=False):
                         reverse=True
                     )[0]
                     if cmpResult == 2:
-                        result = compareVersions(relName,
-                                                 latestPreRelease["name"])
+                        result = compareVersions(
+                            relName,
+                            latestPreRelease["name"]
+                        )
                     else:
-                        result = compareVersions(eg.Version.string,
-                                                 latestPreRelease["name"])
+                        result = compareVersions(
+                            eg.Version.string,
+                            latestPreRelease["name"]
+                        )
                     if result == 2:
                         cmpResult = 2
                         relName = latestPreRelease["name"]
                         relUrl = latestPreRelease["html_url"]
-
-
         if dialog:
             dialog.Destroy()
             dialog = None
-
         if cmpResult == 2:
             wx.CallAfter(MessageDialog, relName, relUrl)
         else:
@@ -215,146 +220,65 @@ class CheckUpdate:
     def CheckUpdateManually(cls):
         _checkUpdate(manually=True)
 
-###  The following code is from QGIS
 
-"""
-/***************************************************************************
-                        Plugin Installer module
-                        Plugin version comparision functions
-                             -------------------
-    Date                 : 2008-11-24
-    Copyright            : (C) 2008 by Borys Jurgiel
-    Email                : info at borysjurgiel dot pl
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
-
-Here is Python function for comparing version numbers. It's case insensitive
-and recognizes all major notations, prefixes (ver. and version), delimiters
-(. - and _) and suffixes (alpha, beta, rc, preview and trunk).
-
-Usage: compareVersions(version1, version2)
-
-The function accepts arguments of any type convertable to unicode string
-and returns integer value:
-0 - the versions are equal
-1 - version 1 is higher
-2 - version 2 is higher
-
------------------------------------------------------------------------------
-HOW DOES IT WORK...
-First, both arguments are converted to uppercase unicode and stripped of
-'VERSION' or 'VER.' prefix. Then they are chopped into a list of particular
-numeric and alphabetic elements. The dots, dashes and underlines are recognized
-as delimiters. Also numbers and non numbers are separated. See example below:
-
-'Ver 0.03-120_rc7foo' is converted to ['0','03','120','RC','7','FOO']
-
-Then every pair of elements, from left to right, is compared as string
-or as number to provide the best result (you know, 11>9 but also '03'>'007').
-The comparing stops when one of elements is greater. If comparing achieves
-the end of the shorter list and the matter is still unresolved, the longer
-list is usually recognized as higher, except following suffixes:
-ALPHA, BETA, RC, PREVIEW and TRUNK which make the version number lower.
-"""
-
-
-def normalizeVersion(s):
-    """ remove possible prefix from given string and convert to uppercase """
-    prefixes = ['VERSION', 'VER.', 'VER', 'V.', 'V', 'REVISION', 'REV.', 'REV', 'R.', 'R']
-    if not s:
-        return unicode()
-    s = unicode(s).upper()
-    for i in prefixes:
-        if s[:len(i)] == i:
-            s = s.replace(i, '')
-    s = s.strip()
-    return s
-
-
-def classifyCharacter(c):
-    """ return 0 for delimiter, 1 for digit and 2 for alphabetic character """
-    if c in [".", "-", "_", " "]:
-        return 0
-    if c.isdigit():
-        return 1
-    else:
-        return 2
-
-
-def chopString(s):
-    """ convert string to list of numbers and words """
-    l = [s[0]]
-    for i in range(1, len(s)):
-        if classifyCharacter(s[i]) == 0:
-            pass
-        elif classifyCharacter(s[i]) == classifyCharacter(s[i - 1]):
-            l[len(l) - 1] += s[i]
-        else:
-            l += [s[i]]
-    return l
-
-
-def compareElements(s1, s2):
-    """ compare two particular elements """
-    # check if the matter is easy solvable:
-    if s1 == s2:
-        return 0
-    # try to compare as numeric values (but only if the first character is not 0):
-    if s1 and s2 and s1.isnumeric() and s2.isnumeric() and s1[0] != '0' and s2[0] != '0':
-        if float(s1) == float(s2):
-            return 0
-        elif float(s1) > float(s2):
-            return 1
-        else:
-            return 2
-    # if the strings aren't numeric or start from 0, compare them as a strings:
-    # but first, set ALPHA < BETA < PREVIEW < RC < TRUNK < [NOTHING] < [ANYTHING_ELSE]
-    if s1 not in ['ALPHA', 'BETA', 'PREVIEW', 'RC', 'TRUNK']:
-        s1 = 'Z' + s1
-    if s2 not in ['ALPHA', 'BETA', 'PREVIEW', 'RC', 'TRUNK']:
-        s2 = 'Z' + s2
-    # the final test:
-    if s1 > s2:
-        return 1
-    else:
-        return 2
-
-
-def compareVersions(a, b):
+def compareVersions(ver_a, ver_b):
     """ Compare two version numbers. Return 0 if a==b or error, 1 if a>b and 2 if b>a """
-    if not a or not b:
+    if not ver_a or not ver_b:
         return 0
-    a = normalizeVersion(a)
-    b = normalizeVersion(b)
-    if a == b:
-        return 0
-    # convert the strings to lists
-    v1 = chopString(a)
-    v2 = chopString(b)
-    # set the shorter string as a base
-    l = len(v1)
-    if l > len(v2):
-        l = len(v2)
-    # try to determine within the common length
-    for i in range(l):
-        if compareElements(v1[i], v2[i]):
-            return compareElements(v1[i], v2[i])
-    # if the lists are identical till the end of the shorther string, try to compare the odd tail
-    #with the simple space (because the 'alpha', 'beta', 'preview' and 'rc' are LESS then nothing)
-    if len(v1) > l:
-        return compareElements(v1[l], u' ')
-    if len(v2) > l:
-        return compareElements(u' ', v2[l])
-    # if everything else fails...
+    try:
+        a = Version(ver_a)
+        b = Version(ver_b)
+    except InvalidVersion:
+        return -1
+
     if a > b:
         return 1
-    else:
+    elif b > a:
         return 2
+    return 0
+
+
+class InvalidVersion(Exception):
+    pass
+
+
+def ParseVersion(ver):
+    match = re.search(
+        "^v?(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)"
+        "(-(?P<pre>:alpha|beta|rc)(?P<pre_nr>\d+))?$",
+        ver.strip().lower()
+    )
+    if match:
+        return match.groupdict()
+    else:
+        raise InvalidVersion
+
+
+class Version:
+    def __init__(self, ver_str):
+        ver = ParseVersion(ver_str)
+        self.ver_str = "{0:04}{1:04}{2:04}{3}{4:04}".format(
+            int(ver["major"]),
+            int(ver["minor"]),
+            int(ver["patch"]),
+            ver["pre"] if ver["pre"] else "z",
+            int(ver["pre_nr"]) if ver["pre_nr"] else 9999
+        )
+
+    def __lt__(self, other):
+        return self.ver_str < other.ver_str
+
+    def __le__(self, other):
+        return self.ver_str <= other.ver_str
+
+    def __gt__(self, other):
+        return self.ver_str > other.ver_str
+
+    def __ge__(self, other):
+        return self.ver_str >= other.ver_str
+
+    def __eq__(self, other):
+        return self.ver_str == other.ver_str
+
+    def __ne__(self, other):
+        return self.ver_str != other.ver_str
