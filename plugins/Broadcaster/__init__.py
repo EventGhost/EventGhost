026ab89277c5bb3efd132600a7e760de5cae17c7
David Mark <david.mark@cinsoft.biz>
2016-11-20 10:12:09 -0500

    Pass only decoded Unicode strings to TriggerEvent
    Passing raw bytes received from UDP causes exceptions for non-Ascii characters. Change also allows for non-Ascii characters for the payload delimiter (previously caused an exception due to str() call).
    
    See further explanation here:
    
    http://www.eventghost.net/forum/viewtopic.php?f=4&t=8222&p=43750#p43750

--- plugins/Broadcaster/__init__.py
+++ plugins/Broadcaster/__init__.py
@@ -89,7 +89,8 @@ class Server(asyncore.dispatcher):
         my_addr = addr[0] in self.addresses
 
         if (not my_addr) or self.selfBroadcast:
-            bits = data.split(str(self.payDelim))
+            bits = data.split(self.payDelim)
+            bits = [bit.decode('utf-8') for bit in bits]
             commandSize=len(bits)
             if commandSize==1:
                 self.plugin.TriggerEvent(bits[0])
