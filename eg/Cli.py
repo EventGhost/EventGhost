92e597037fe56c62f00cac18531c115464e4ccb6
Kevin Schlosser <drschlosser@hotmail.com>
2017-03-11 23:07:12 -0600

    Updates help file and /? to be the same

--- eg/Cli.py
+++ eg/Cli.py
@@ -41,77 +41,115 @@ import __main__  # NOQA
 isMain = hasattr(__main__, "isMain")
 
 
-HELP = '''
+HELP = '''Command Line Options
+====================
+
 Usage:
     eventghost [.egtree/.egplugin file]
-               [-?,   --help] [-h, --hide] [-i, --install] [-u, --uninstall]
-               [-m, --multiload] [-t, --translate] [-r, --restart]
-               [-n,   --netsend [data]] [-d, --debug [modules]]
-               [-e,   --event [event] [payload]] [-c, --configdir [config path]]
-               [-p,   --pluginfile [.egplugin file]] [-f, --file [.egtree file]]
+               [-?, /?, -help, --help] [-h, /h, -hide, --hide]
+               [-i, /i, -install, --install] [-u, /u, -uninstall, --uninstall]
+               [-m, /m, -multiload, --multiload] [-t, /t, -translate, --translate]
+               [-r, /r, -restart, --restart]
+               [-n, /n, -netsend, --netsend <host>:<port> <password> <eventname> [<payload>]]
+               [-d, /d, -debug, --debug <modules>]
+               [-e, /e, -event, --event <event> [<payload>]
+               [-c, /c, -configdir, --configdir <config path>]
+               [-f, /f, -file, --file <.egtree file>]
+               [-p, /p, -pluginFile, --pluginfile <.egplugin file>]
 
+The EventGhost main executable accepts the following command line arguments:
 
-    .egtree/.egplugin file
-        will load a save file or install a plugin
-        eventghost C:\SomeDirectory\SomeFile.(egtree/egplugin)
+.egtree/.egplugin file
+    Will load a save file or install a plugin
+    eventghost Saved_Data.egtree
 
-    -?,   --help              Show this help message and exit.
-    -h,   --hide             Start EventGhost minimized.
-    -i,    --install           Compile all EventGhost files.
-    -u,   --uninstall      Remove all .pyc (python compiled) files.
-    -m, --multiload     Open multiple instances of EventGhost.
-    -t,    --translate      Open Translation Editor.
-    -r,    --restart          Restart EventGhost.
-    -n,   --netsend       Send data to another computer.
-                                     --netsend "Some data to be sent"
+-?, /?, -help, --help
+    Show this help message and exit.
 
-    -d,   --debug          Enable debugging. Optionally you can specify
-                                     module names to enable verbose debugging.
-                                     If the module supports verbose debugging.
+-h, /h, -hide, --hide
+    Start EventGhost minimized.
 
-                                     enable verbose debugging globally.
-                                     --debug eg
+-i, /i, -install, --install
+    Compile all EventGhost files.
 
-                                     enable debugging for all core plugins.
-                                     --debug eg.CorePluginModule
+-u, /u, -uninstall, --uninstall
+    Remove all .pyc (python compiled) files.
 
-                                     enable debugging for a specific core
-                                     plugin.
-                                     --debug eg.CorePluginModule.EventGhost
+-m, /m, -multiload, --multiload
+    Open multiple instances of EventGhost.
 
-                                     enable debugging for a specific module in
-                                     a core plugin.
-                                     --debug eg.CorePluginModule.Window.SendKeys
+-t, /t, -translate, --translate
+    Starts EventGhost's translation editor.
 
-                                     You can also specify more then one module
-                                     to set to verbose debugging just put a
-                                     space between them. Because this feature
-                                     uses a "Bottom Up" means to set the
-                                     verbose debugging doing the following is
-                                     pointless.
-                                     --debug eg.CorePluginModule eg
+-r, /r, -restart, --restart
+    Restart EventGhost.
 
-                                     This is because eg is the bottom most
-                                     module and everything on top of it also
-                                     has verbose debugging set.
+-d, /d, -debug, --debug <module names>
+    Enable debugging. Optionally you can specify module names to enable verbose
+    debugging. If the module supports verbose debugging.
 
-    -e,    --event            Trigger an event.
-                                     --event Some.Triggered.Event PayloadData
+    To enable verbose debugging globally.
+    --debug eg
 
-    -c,   --configdir      Specify what config file to use.
-                                     --configdir C:\SomeDirectory
+    Enable debugging for all core plugins.
+    --debug eg.CorePluginModule
 
-    -p,   --pluginfile     Install a plugin (.egplugin).
-                                     --pluginfile C:\SomeDir\PluginFile.egplugin
+    Enable debugging for a specific core plugin.
+    --debug eg.CorePluginModule.EventGhost
 
-    -f,   --file           Specify which .egtree (save file) to load.
-                                     --file C:\SomeDir\SaveFile.egtree
+    Enable debugging for a specific module in a core plugin.
+    --debug eg.CorePluginModule.Window.SendKeys
 
-    ** Now don't forget if you want an optional argument that has spaces in it
-    to be treated as a single statement, you will need to wrap the statement in
-    "double quotes"
+    You can also specify more then one module to set to verbose debugging just
+    put a space between them. Because this feature uses a "Bottom Up" means to
+    set the verbose debugging.
 
+    Doing the following is pointless.
+    --debug eg.CorePluginModule eg
+
+    This is because eg is the bottom most module and everything on top of it
+    also has verbose debugging set.
+
+-e, /e, -event, --event <eventname> [<payload>]
+    Trigger an event with optional payload.
+
+    Issues the event <eventname> in the currently running EventGhost instance.
+    Optionally you can specify one or more <payload> strings, that will be
+    added to the event in the eg.event.payload <eg.EventGhostEvent.payload>
+    field.
+
+-n, /n, -netsend, --netsend <host>:<port> <password> <eventname> [<payload>]
+    Send an event and an optional payload to another computer running
+    EventGhost.
+
+    This one is similar to the -event option, but sends the event
+    <eventname> through TCP/IP like the 'Network Event Sender' plugin does. It
+    will not start EventGhost, so it can be used as a little helper tool for
+    other applications or .BAT files to send events to a remote machine.
+    <host> has to be the IP or host name of the target machine. <port> and
+    <password> are the options that you have configured on the target
+    machine's 'Network Event Receiver' plugin.
+
+-c, /c, -configdir, --configdir <directory>
+    Specify what config file to use.
+
+    Instructs EventGhost to use the directory <directory> to store and
+    retrieve its settings. Without this option EventGhost uses a directory in
+    the application data folder of your machine for storing its settings.
+    For example, through this option you can change the folder to a location
+    on a USB stick to make EventGhost portable.
+
+-p, /p, -pluginfile, --pluginfile <.egplugin file>
+    Install a plugin.
+
+-f, /f, -file, --file  <.egtree file>
+    Specify save file to load.
+
+** Now don't forget if you want an optional argument that has spaces in it
+to be treated as a single statement, you will need to wrap the statement in
+"double quotes"
 '''
+
 
 class StdOut(object):
 
@@ -129,7 +167,7 @@ class StdOut(object):
             self.app.MainLoop()
             self.dialog = wx.Dialog(
                 None,
-                size=(475, 600),
+                size=(600, 700),
                 title='Command Line Help',
                 style=(
                     wx.CAPTION |
