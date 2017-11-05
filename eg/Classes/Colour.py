292c9c0cc1ccbb03caebdccd967a2bedacaf9bf5
Kevin Schlosser <drschlosser@hotmail.com>
2017-11-05 10:09:41 -0700

    Another try at the colors.

--- eg/Classes/Colour.py
+++ eg/Classes/Colour.py
@@ -44,12 +44,12 @@ class Colour:
     windowBackground = wx.SystemSettings_GetColour(wx.SYS_COLOUR_WINDOW).Get()
     treeItem = windowText
     pluginError = (255, 0, 0)
-    errorText = (255, 0, 0)
-    errorBackground = windowBackground
+    errorText = (0, 0, 0)
+    errorBackground = (237, 80, 83)
     warningText = (0, 0, 0)
-    waningBackground = (242, 137, 0)
+    waningBackground = (239, 182, 112)
     debugText = (0, 0, 0)
-    debugBackground = (0, 200, 0)
+    debugBackground = (165, 217, 255)
     activeCaption = GetWinSysColour(COLOR_ACTIVECAPTION)
     activeCaptionGradient = GetWinSysColour(COLOR_GRADIENTACTIVECAPTION)
     activeCaptionTextColour = GetWinSysColour(COLOR_CAPTIONTEXT)
