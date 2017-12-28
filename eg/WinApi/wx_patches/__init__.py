52fa41cfeb40ae5dfe914c96500a34579e415b79
Kevin Schlosser <drschlosser@hotmail.com>
2017-12-28 10:46:16 -0700

    Changes the imports and setting the new class in place

--- eg/WinApi/wx_patches/__init__.py
+++ eg/WinApi/wx_patches/__init__.py
@@ -17,20 +17,10 @@
 # with EventGhost. If not, see <http://www.gnu.org/licenses/>.
 
 
-# monkey patch to correct the setting of the value
-# following possible errors:
-#
-# masked.NumCtrl(value=5, min=5)
-#   ValueError: value 0 is below minimum value of control
-# masked.NumCtrl(value=-15, max=-25)
-#   ValueError: value 0 exceeds value of control
-
 import sys
-import __builtin__
-import wx.lib.masked
-import wx.lib.masked.numctrl
+from wx.lib.masked import NumCtrl
 
-_num_ctrl = wx.lib.masked.numctrl.NumCtrl
+_num_ctrl = NumCtrl
 
 
 class _NumCtrl(_num_ctrl):
@@ -38,18 +28,13 @@ class _NumCtrl(_num_ctrl):
     def __init__(self, parent, id=-1, value=None, *args, **kwargs):
         self._hold_value = value
         self.SetParameters = self._set_parameters
-        _num_ctrl.__init__(self, parent, id, value, *args, **kwargs)
+        super(_NumCtrl, self).__init__(self, parent, id, value, *args, **kwargs)
 
     def _set_parameters(self, **kwargs):
         self.SetValue(self._hold_value)
         _num_ctrl.SetParameters(self, **kwargs)
         self.SetParameters = _num_ctrl.SetParameters
 
-wx.lib.masked.numctrl.NumCtrl = _NumCtrl
 
-sys.modules['wx.lib.masked.numctrl'].NumCtrl = wx.lib.masked.numctrl.NumCtrl
-sys.modules['wx.lib.masked'].numctrl.NumCtrl = wx.lib.masked.numctrl.NumCtrl
-sys.modules['wx.lib'].masked.numctrl.NumCtrl = wx.lib.masked.numctrl.NumCtrl
-sys.modules['wx'].lib.masked.numctrl.NumCtrl = wx.lib.masked.numctrl.NumCtrl
-
-__builtin__.wx = sys.modules['wx']
+sys.modules['wx.lib.masked.numctrl'].NumCtrl = _NumCtrl
+sys.modules['wx.lib.masked'].NumCtrl = _NumCtrl
