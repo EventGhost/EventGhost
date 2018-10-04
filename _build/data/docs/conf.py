32b465aee80a781e2c91182254e3e343db456f35
topic2k <topic2k+git@gmail.com>
2018-10-04 21:34:21 +0200

    wrong filename for favicon

--- _build/data/docs/conf.py
+++ _build/data/docs/conf.py
@@ -145,7 +145,7 @@ html_logo = os.path.join(root, 'images', 'logo.png')
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
-html_favicon = r"_static\logo.ico"
+html_favicon = r"_static\arrow.ico"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
