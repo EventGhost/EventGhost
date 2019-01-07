8243c9ae130140f761e5cdf04366fb835611208a
Markus Gruber <GruberMarkus@users.noreply.github.com>
2019-01-07 19:00:51 +0100

    [VLC] add 64 bit support, fix play, volup and voldown (#341)
    - Add support for 64 bit VLC install directory detection via a registry query. 
    - Switch to socket.sendall, as pure sendall is now deprecated.
    - "volup" and "voldown" are no longer supported by VLC as standalone commands, they now require a parameter with the number of steps by which to increment or decrement the volume. E.g., "volup 1" to increase volume by one step.
    - The play command now does what it says, that is start media playback. It no longer toggles between play and pause states, as there is a separate command for this.

--- plugins/VLC/__init__.py
+++ plugins/VLC/__init__.py
@@ -37,8 +37,8 @@ are enabled in VLC!
 
 eg.RegisterPlugin(
     name = "VLC media player",
-    author = "MonsterMagnet",
-    version = "0.4.1486",
+    author = "MonsterMagnet, GruberMarkus",
+    version = "0.5",
         kind = "program",
     guid = "{02929D1C-7567-414C-84D1-F8D71D6FD7B3}",
     canMultiLoad = True,
@@ -82,18 +82,19 @@ def GetVlcPath():
     """
     Tries to get the path of VLC's executable through querying the Windows
     registry.
+    Not querying HKLM\Software\VideoLAN\VLC to avoid multiple queries
+    neccessary due to registry virtualization on 64 bit systems.
     """
     try:
-        return _winreg.QueryValue(
-            _winreg.HKEY_LOCAL_MACHINE,
-            "Software\\VideoLAN\\VLC"
-        )
-    except WindowsError:
-        return os.path.join(
-            eg.folderPath.ProgramFiles,
-            "VideoLAN\\VLC\\vlc.exe"
-        )
+        path = _winreg.QueryValue(_winreg.HKEY_LOCAL_MACHINE, "SOFTWARE\Classes\VLC.OPENFolder\shell\Open\command").split("\"")[1]
+    except:
+        return None
 
+    if os.path.exists(path):
+            return path
+    else:
+        return None
+            
 
 def GetChoices():
     f = codecs.open(eg.folderPath.RoamingAppData+"\\vlc\\vlcrc",'r','utf-8')
@@ -133,7 +134,7 @@ class VlcSession(asynchat.async_chat):
     def handle_connect(self):
         # connection succeeded
         self.plugin.TriggerEvent("Connected")
-        self.sendall(self.plugin.connectedevent)
+        self.socket.sendall(self.plugin.connectedevent)
 
 
     def handle_expt(self):
@@ -616,7 +617,7 @@ class VLC(eg.PluginBase):
             self.isDispatcherRunning = True
         try:
             if self.dispatcher.connected:
-                self.dispatcher.sendall(data)
+                self.dispatcher.socket.sendall(data)
             return True
         except:
             self.isDispatcherRunning = False
@@ -669,7 +670,7 @@ ACTIONS = (
         'Play',
         'Play',
         'Start playing',
-        'pause'
+        'play'
     ),
     (
         ActionPrototype,
@@ -812,14 +813,14 @@ ACTIONS = (
         'VolumeUp',
         'Volume Up',
         'Volume up',
-        'volup'
+        'volup 1'
     ),
     (
         ActionPrototype,
         'VolumeDown',
         'Volume Down',
         'Volume down',
-        'voldown'
+        'voldown 1'
     ),
     (
         MyCommand,
